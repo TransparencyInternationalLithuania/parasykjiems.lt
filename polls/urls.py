e5181b8d0a5086f78bdde4716175898a00d171c6
Darius Damalakas <darius.damalakas@gmail.com>
2010-05-27 14:51:37 +0300

    Decoupling polls from whole app

--- polls/urls.py
+++ polls/urls.py
@@ -4,17 +4,16 @@ from django.conf.urls.defaults import *
 from django.contrib import admin
 admin.autodiscover()
 
-
-
-urlpatterns = patterns('',
+urlpatterns = patterns('parasykjiems.polls.views',
 	# Example:
 	# (r'^parasykjiems/', include('parasykjiems.foo.urls')),
 
 	# Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
 	# to INSTALLED_APPS to enable admin documentation:
 	# (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-	(r'^polls/', include('parasykjiems.polls.urls')),
 
-	# Uncomment the next line to enable the admin:
-	(r'^admin/', include(admin.site.urls)),
+	(r'^$', 'index'),
+	(r'^(?P<poll_id>\d+)/$', 'detail'),
+	(r'^(?P<poll_id>\d+)/results/$', 'results'),
+	(r'^(?P<poll_id>\d+)/vote/$', 'vote'),
 )
