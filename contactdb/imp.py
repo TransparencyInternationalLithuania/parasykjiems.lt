a182055f346e5985f33d8851e8b636225471213a
Darius Damalakas <darius.damalakas@gmail.com>
2010-05-28 18:15:00 +0300

    Creating properties

--- contactdb/imp.py
+++ contactdb/imp.py
@@ -1,11 +1,24 @@
 
 municipalities = "sources/apygardos.txt"
+a = "sources/a.txt"
 
 class State:
     District = "d"
     County = "c"
     ElectionDistrict = "ec"
     
+class MunicipalityLocation:
+    def __init__(self):
+        self._district = ""
+    
+    @property
+    def District(self):
+        return self._x;
+
+    @District.setter
+    def District(self, value):
+        self._district = value
+        
 
 
 # a generator function which returns only non empty
@@ -19,20 +32,22 @@ def notEmptyLines(file):
         # return each splitted line as separate line
         for s in lines:
             s = s.strip("* ")
-            if (s == ""):
-                continue
             yield s
+
+
     
 def myF(file):
     state = State.District
     infile = open(file, "r")
 
     for line in notEmptyLines(infile):
-    
+
+        print line
         if state == State.District:
             districtName = line
             state = State.County
-            pass
+            print "district " + districtName
+            continue
 
         if (state == State.County):
             countyName = line
