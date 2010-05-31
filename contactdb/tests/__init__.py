3d581797cde08e0a8b4309c7bf23aa149bb78634
Darius Damalakas <darius.damalakas@intelliflo.com>
2010-05-31 14:06:35 +0300

    Running first tests

---
+++ contactdb/tests/__init__.py
@@ -0,0 +1,7 @@
+from __future__ import absolute_import
+
+from .tests import *
+
+__test__ = {
+    'SimpleTest' : SimpleTest,
+}
---
+++ contactdb/tests/tests.py
@@ -0,0 +1,36 @@
+﻿from django.test import TestCase
+from parasykjiems.contactdb.imp import *
+import os
+
+
+scriptPath = os.path.dirname( os.path.realpath( __file__ ) )
+
+class SimpleTest(TestCase):
+    singleRecordFile = scriptPath + "/a.txt"
+
+    def test_basic_addition(self):
+        """
+        Tests that 1 + 1 always equals 2.
+        """
+        self.failUnlessEqual(1 + 1, 2)
+
+    def test_read_single_record_count(self):
+        file = open(self.singleRecordFile, "r")
+        count = 0
+        for l in getLocations(file):
+            count += 1
+
+        self.assertEqual(count, 1)
+
+    def test_read_single_record(self):
+
+        file = open(self.singleRecordFile, "r")
+        for loc in getLocations(file):
+            self.assertEqual(loc.District, "Akmenės rajonAS")
+            self.assertEqual(loc.County, "Akmenės–Joniškio rinkimų apygarda Nr. 39")
+            self.assertEqual(loc.ElectionDistrict, "Senamiesčio rinkimų apylinkė Nr. 1")
+            self.assertTrue(loc.Addresses.index("Naujoji Akmenė: Algirdo g., Aušros g., Barvydžio vs.,") >= 0)
+
+
+
+
