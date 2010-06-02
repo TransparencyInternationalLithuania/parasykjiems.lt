c9f4fabfda58fa6a7096b9aa8c66eea515a158a6
Darius Damalakas <darius.damalakas@intelliflo.com>
2010-06-02 12:43:19 +0300

    broken commit

--- contactdb/tests/__init__.py
+++ contactdb/tests/__init__.py
@@ -1,7 +1,9 @@
 from __future__ import absolute_import
 
 from .tests import *
+from .TestReadParliamentMembers import *
 
 __test__ = {
     'SimpleTest' : SimpleTest,
+    'TestReadParliamentMembers' : TestReadParliamentMembers
 }
