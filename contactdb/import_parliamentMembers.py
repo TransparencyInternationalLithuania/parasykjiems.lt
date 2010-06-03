a4ba806c5751509d562791f62bf39f82c9c9c52e
Darius Damalakas <darius.damalakas@gmail.com>
2010-06-03 16:38:21 +0300

    Reading parliament members

--- contactdb/import_parliamentMembers.py
+++ contactdb/import_parliamentMembers.py
@@ -1,25 +1,52 @@
-﻿import os
-from parasykjiems.contactdb.models import *
+
+from contactdb.models import ParliamentMember
+from contactdb.tests.exc import ChainnedException
+
+
+class ParliamentMemberImportError(ChainnedException):
+    def __init__(self, message, inner = None):
+        ChainnedException.__init__(self, message, inner)
+
+
 
 def ReadLine(file):
     return file.readline().strip("/n")
+
+def readField(fields, index):
+    if (index >= len(fields)):
+        return ""
+    return fields[index]
 
 def ReadParliamentMembers(file):
     """A generator which returns parliament member instances from
 given file"""
     header = ReadLine(file)
 
+    count = 0
     for line in file:
         if (line == ""):
             break
+        count += 1 
         fields = line.split("\t")
 
+
         member = ParliamentMember()
-        member.electoralDistrict = fields[0]
-        member.name = fields[2]
-        member.surname = fields[1]
-        member.email = fields[6]
+        member.electoralDistrict = readField(fields, 0)
+        member.name = readField(fields, 2)
+        member.surname = readField(fields, 1)
+        member.email = readField(fields, 6)
+
+        try:
+            validateMember(member)
+        except ParliamentMemberImportError as e:
+            raise ParliamentMemberImportError("error importing line \n %(lineNumber)s \n %(line)s" % {'lineNumber' : count, 'line': line}, e)
+
         yield member
+
+def validateMember(member):
+    if member.electoralDistrict.lower().find("nr") < 0:
+        raise ParliamentMemberImportError("ElectoralDistrict field does not contain number. It was '%(fieldName)s', and it did not contain letters 'nr' See stack trace for more details" % {'fieldName' : member.electoralDistrict})
+
 
 #file = open("parliament members.txt", "r")
 #for p in ReadParliamentMembers(file):
