39c6b3431fc984a7229ff6e8556c51b18e394de1
kroitus <kroitus@localhost>
2010-06-27 01:27:49 +0300

    Started to work on address search

--- pjweb/views.py
+++ pjweb/views.py
@@ -2,6 +2,7 @@
 from django import forms
 from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render_to_response
+from parasykjiems.contactdb.models import CountyStreet, County, ParliamentMember
 
 class ContactForm(forms.Form):
     subject = forms.CharField(max_length=100)
@@ -12,17 +13,39 @@ class IndexForm(forms.Form):
     address = forms.CharField(max_length=255)
 
 def index(request):
-    address = ''
+    entered = ''
+    suggestions = []
     if request.method == 'POST':
         form = IndexForm(request.POST)
         if form.is_valid():
-            address = form.cleaned_data['address']
-
+            entered = form.cleaned_data['address']
+        entered_list = entered.split(', ')
+        if len(entered_list)>1:
+            entered_street = entered_list[0].split(' ')
+            filtered_addr = CountyStreet.objects.all().filter(
+                city__startswith=entered_list[1],
+                street__startswith=entered_street[0]
+            )
+            if not filtered_addr:
+                entered_street = entered_list[1].split(' ')
+                filtered_addr = CountyStreet.objects.all().filter(
+                    city__startswith=entered_list[0],
+                    street__startswith=entered_street[0]
+                )
+        else:
+            entered_street = entered_list[0].split(' ')
+            filtered_addr = CountyStreet.objects.all().filter(
+                street__startswith=entered_street[0]
+            )
+        for entry in filtered_addr:
+            entry_string = '%s, %s, %s' %(entry.street, entry.city, entry.district)
+            suggestions.append(entry_string)
     else:
         form = IndexForm()
     return render_to_response('pjweb/index.html', {
         'form': form,
-        'address': address,
+        'entered': entered,
+        'suggestions': suggestions,
     })
 #    return HttpResponse("Hello, world. You're at the index.")
 
