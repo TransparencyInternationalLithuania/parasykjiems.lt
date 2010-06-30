0af8aba339feb3ccd1c0db6c4c0cea241a12bb57
Vytautas Butenas <928358+kroitus@users.noreply.github.com>
2010-06-30 12:45:49 +0300

    Improved search function

--- pjutils/address_search.py
+++ pjutils/address_search.py
@@ -15,12 +15,24 @@ class AddressSearch:
             )
     	return filtered
 
+    def isnumeric(self, number):
+        numbers = number.split('-')
+        try:
+            isnum = float(numbers[-1])
+        except:
+            return False
+        if not isnum:
+            return False
+        else:
+            return True
+
     def get_addr_suggests(self, table, entered):
         suggestions = []
         entered_list = entered.split(', ')
         if len(entered_list)>1:
             street_list = entered_list[0].split(' ')
-            street_list.pop()
+            if self.isnumeric(street_list[-1]):
+                street_list.pop()
             street = ' '.join(street_list)
             city = entered_list[1]
             if len(entered_list)>2:
@@ -30,13 +42,20 @@ class AddressSearch:
             records = self.search_for_addr(table.objects.all(), district, 'district')
             records = self.search_for_addr(records, city, 'city')
             records = self.search_for_addr(records, street, 'street')
+            if not records:
+                records = self.search_for_addr(table.objects.all(), city, 'district')
+                if not records:
+                    records = self.search_for_addr(table.objects.all(), street, 'city')
+                else:
+                    records = self.search_for_addr(records, street, 'city')
         else:
             street_list = entered_list[0].split(' ')
-            street_list.pop()
+            if self.isnumeric(street_list[-1]):
+                street_list.pop()
             street = ' '.join(street_list)
+            print street
             records = self.search_for_addr(table.objects.all(), street, 'street')
+            if not records:
+                records = self.search_for_addr(table.objects.all(), street, 'city')
 
-        for entry in records:
-            entry_string = '%s, %s, %s' %(entry.street, entry.city, entry.district)
-            suggestions.append(entry_string)
-        return suggestions
+        return records
