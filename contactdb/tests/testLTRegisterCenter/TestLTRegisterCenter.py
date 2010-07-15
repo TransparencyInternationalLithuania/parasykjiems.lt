cd6ecedfd8946d13c487a330635a5beebd9c7a93
dariusdamalakas <dariusdamalakas@localhost>
2010-07-15 23:42:41 +0300

    Adding test to check that first page location is extracted ok

--- contactdb/tests/testLTRegisterCenter/TestLTRegisterCenter.py
+++ contactdb/tests/testLTRegisterCenter/TestLTRegisterCenter.py
@@ -5,6 +5,7 @@ from django.test import TestCase
 import os
 from contactdb.AdressParser import AddressParser
 from contactdb.imp import LithuanianConstituencyReader
+from contactdb.LTRegisterCenter.webparser import RegisterCenterParser, RegisterCenterPage
 
 scriptPath = os.path.dirname( os.path.realpath( __file__ ) )
 
@@ -16,5 +17,8 @@ class TestLTRegisterCenter(TestCase):
         pass
 
     def testLietuvosRespublika(self):
-        pass
-        
+        file = open(self.LietuvosRespublikaHtml)
+        lines = "".join(file.readlines())
+        page = RegisterCenterParser(lines).parse()
+        self.assertEqual(1, len(page.location))
+        self.assertEqual("LIETUVOS RESPUBLIKA", page.location[0])
