d729b4ba757e4120e3454307c71c964322d339bc
Darius Damalakas <darius.damalakas@intelliflo.com>
2010-07-16 00:24:46 +0300

    Adding more tests for parsing locations

--- contactdb/LTRegisterCenter/webparser.py
+++ contactdb/LTRegisterCenter/webparser.py
@@ -1,6 +1,7 @@
 from urllib2 import urlopen
 from ClientForm import ParseResponse
 from BeautifulSoup import BeautifulSoup
+import re
 
 class RegisterCenterPage:
     location = []
@@ -9,18 +10,34 @@ class RegisterCenterParser:
     def __init__(self, htmlText):
         self.htmlText = htmlText
 
+    def _removeLineBreaks(slef, string):
+        rootLocation = re.sub(r'\n', ' ', str(string))
+        rootLocation = rootLocation.replace("   ", " ")
+        return rootLocation
+
     def GetLocation(self, lt):
         location = []
-        location.append(lt)
+
+        # always add first location. also remove any \n characters with regexp
+
+        location.append(self._removeLineBreaks(lt))
+
+        # if first location is contained in bold tag, then no other tags will be present
         if (lt.parent.name == 'b'):
             return location
 
+        # otherwise get the encapsulated a tag
         root = lt.parent.parent
-        while (True):
-            root = root.findNext().findNext()
-            nextName = root.next.next
-            location.append(nextName)
 
+        while (True):
+            # get the next <a> tag
+            root = root.findNext().findNext()
+
+            # add location name
+            nextName = root.next.next
+            location.append(self._removeLineBreaks(nextName))
+
+            # check if this is the last tag by searching for <br> tags
             brTag = root.next.next.next
             if (hasattr(brTag, "name") == False):
                 continue
@@ -32,10 +49,10 @@ class RegisterCenterParser:
         soupForm = BeautifulSoup(self.htmlText)
 
 
-        lt = soupForm.find(text="LIETUVOS RESPUBLIKA")
+        lt = soupForm.find(text=re.compile("LIETUVOS|RESPUBLIKA"))
 
         if (lt is None):
-            print "Could not find 'Lietuvos Respublika' tag, page has been changed. Damn"
+            print "Could not find 'LIETUVOS RESPUBLIKA' tag, page has been changed. Damn"
             return
         page = RegisterCenterPage()
         page.location = self.GetLocation(lt)
