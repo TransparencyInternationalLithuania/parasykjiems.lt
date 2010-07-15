60d0ae0b6b66e3361691ebdba3260efef9e5e064
Darius Damalakas <darius.damalakas@intelliflo.com>
2010-07-15 23:42:41 +0300

    Adding test to check that first page location is extracted ok

--- contactdb/tests/__init__.py
+++ contactdb/tests/__init__.py
@@ -2,10 +2,12 @@ from __future__ import absolute_import
 from contactdb.tests.TestReadParliamentMembers import TestReadParliamentMembers
 from contactdb.tests.TestImportLithuanianCounties import TestImportLithuanianCounties, TestLithuanianConstituencyParser
 from contactdb.tests.TestAddressParser import TestAddressParser
+from contactdb.tests.testLTRegisterCenter.TestLTRegisterCenter import TestLTRegisterCenter
 
 __test__ = {
     'TestImportLithuanianCounties' : TestImportLithuanianCounties,
     'TestReadParliamentMembers' : TestReadParliamentMembers,
     'TestLithuanianConstituencyParser' : TestLithuanianConstituencyParser,
-    'TestAddressParser' : TestAddressParser
+    'TestAddressParser' : TestAddressParser,
+    'TestLTRegisterCenter' : TestLTRegisterCenter
 }
