b966d684b835b831c2e8a6f606c4722a89646987
dariusdamalakas <dariusdamalakas@localhost>
2010-07-26 21:33:16 +0300

    Parsing seniunaitija streets

--- contactdb/tests/__init__.py
+++ contactdb/tests/__init__.py
@@ -1,6 +1,6 @@
 from __future__ import absolute_import
 from contactdb.tests.TestReadParliamentMembers import TestReadParliamentMembers
-from contactdb.tests.TestImportLithuanianCounties import TestImportLithuanianCounties, TestLithuanianConstituencyParser
+from contactdb.tests.TestImportLithuanianCounties import TestImportLithuanianCounties, TestLithuanianConstituencyParser, TestSeniunaitijaStreetParser
 from contactdb.tests.TestAddressParser import TestAddressParser
 from contactdb.tests.testLTRegisterCenter.TestLTRegisterCenter import TestLTRegisterCenterLocations, TestLTRegisterCenterLinks, TestLTRegisterCenterOtherLinks
 
@@ -11,5 +11,6 @@ __test__ = {
     'TestAddressParser' : TestAddressParser,
     'TestLTRegisterCenterLocations' : TestLTRegisterCenterLocations,
     'TestLTRegisterCenterLinks' : TestLTRegisterCenterLinks,
-    'TestLTRegisterCenterOtherLinks' : TestLTRegisterCenterOtherLinks
+    'TestLTRegisterCenterOtherLinks' : TestLTRegisterCenterOtherLinks,
+    'TestSeniunaitijaStreetParser' : TestSeniunaitijaStreetParser
 }
