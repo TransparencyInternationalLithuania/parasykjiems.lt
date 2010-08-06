90eb7a0b450cac0ca69906ba59b2b184958a9042
Darius Damalakas <darius.damalakas@intelliflo.com>
2010-08-06 16:54:38 +0300

    Updating MPs information based on unique key, not natural key

--- contactdb/management/commands/importMPs.py
+++ contactdb/management/commands/importMPs.py
@@ -14,7 +14,7 @@ class Command(BaseCommand):
 
     def alreadyExists(self, parliamentMember):
         try:
-            ParliamentMember.objects.get(name = parliamentMember.name, surname = parliamentMember.surname)
+            ParliamentMember.objects.get(uniqueKey = parliamentMember.uniqueKey)
         except ObjectDoesNotExist:
             return False
         return True
