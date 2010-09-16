0d0cc006966e652971bbe2526d7521e933dbcf6d
Darius Damalakas <darius.damalakas@intelliflo.com>
2010-09-16 17:02:09 +0300

    Automatically registering all models to admin site

--- contactdb/admin.py
+++ contactdb/admin.py
@@ -1,21 +1,30 @@
 from parasykjiems.contactdb.models import PollingDistrictStreet, Constituency, ParliamentMember
+from parasykjiems.contactdb import models
 from django.contrib import admin
 from django.utils.translation import ugettext as _
+from django.db import models as djangoModel
+import inspect
 
 class AddrAdmin(admin.ModelAdmin):
-
     list_display = ('street', 'city', 'district')
-    
-admin.site.register(PollingDistrictStreet, AddrAdmin)
 
 class MPAdmin(admin.ModelAdmin):
-
     list_display = ('name', 'surname', 'email')
     
-admin.site.register(ParliamentMember, MPAdmin)
-
 class ConstAdmin(admin.ModelAdmin):
-
     list_display = ('name', 'nr')
-    
-admin.site.register(Constituency, ConstAdmin)
+
+# register custom admin models, with custom views
+customModels = [(PollingDistrictStreet, AddrAdmin), (ParliamentMember, MPAdmin), (Constituency, ConstAdmin)]
+for klass, adminKlas in customModels:
+    admin.site.register(klass, adminKlas)
+
+# auto-register all other models
+moduleAttributes = [getattr(models, m) for m in dir(models)]
+moduleClasses = [m for m in moduleAttributes if inspect.isclass(m)]
+modelsList = [m for m in moduleClasses if issubclass(m, djangoModel.Model)]
+# from model list remove already registered models
+for klass, adminKlass in customModels:
+    modelsList.remove(klass)
+# finally register all modules
+admin.site.register(modelsList)
