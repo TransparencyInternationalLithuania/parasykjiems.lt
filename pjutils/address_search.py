8401b77e327d19561c8f0ba4e15029e5469aad65
kroitus <928358+kroitus@users.noreply.github.com>
2010-09-24 01:30:07 +0300

    Fixed bug, which appeared, when first letter was not capitalized and Lithuanian

--- pjutils/address_search.py
+++ pjutils/address_search.py
@@ -25,11 +25,11 @@ class AddressSearch:
         """
         query = None # Query to search for every search term
         terms = self.normalize_query(query_string)
-#        print terms
+        print terms
         for term in terms:
             or_query = None # Query to search for a given term in each field
             for field_name in search_fields:
-                q = Q(**{"%s__icontains" % field_name: term})
+                q = Q(**{"%s__icontains" % field_name: term.capitalize()})
                 if or_query is None:
                     or_query = q
                 else:
@@ -38,4 +38,5 @@ class AddressSearch:
                 query = or_query
             else:
                 query = query & or_query
+
         return query
