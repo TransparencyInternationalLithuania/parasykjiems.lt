a6aa0ab8022627dde52a94a0148576b8681218b5
kroitus <928358+kroitus@users.noreply.github.com>
2010-10-04 00:15:30 +0300

    Selects language (en or lt) by browser pereferences, fixed one search bug

--- pjweb/forms.py
+++ pjweb/forms.py
@@ -10,16 +10,27 @@ from django.core.exceptions import ValidationError
 from parasykjiems.contactdb.models import PollingDistrictStreet, Constituency, ParliamentMember, HierarchicalGeoData, MunicipalityMember, CivilParishMember, SeniunaitijaMember
 from parasykjiems.pjweb.models import Email
 from parasykjiems.pjweb.widgets import *
+from django.utils.safestring import mark_safe
 
 logger = logging.getLogger(__name__)
 
+class HorizontalRadioRenderer(forms.RadioSelect.renderer):
+    """renders horizontal radio buttons.
+    found here:
+    https://wikis.utexas.edu/display/~bm6432/Django-Modifying+RadioSelect+Widget+to+have+horizontal+buttons
+    """
+
+    def render(self):
+        return mark_safe(u'\n'.join([u'%s\n' % w for w in self]))
+
 class ContactForm(forms.Form):
     pub_choices = (
-        ('',''),
         ('private',_('Private')),
         ('public',_('Public')),
     )
-    public = forms.ChoiceField(choices = pub_choices)
+    public = forms.ChoiceField(choices = pub_choices,
+        initial=0,
+        widget=forms.RadioSelect(renderer=HorizontalRadioRenderer))
     sender_name = forms.CharField(max_length=128, validators=[hasNoProfanities])
     phone = forms.CharField(max_length=100, validators=[hasDigits])
     message = forms.CharField(widget=forms.Textarea, validators=[hasNoProfanities])
