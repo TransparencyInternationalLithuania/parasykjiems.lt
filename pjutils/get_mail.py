4c6858c0a8f9e524e0ba1e080126ab708f8ebd00
kroitus <928358+kroitus@users.noreply.github.com>
2010-10-15 01:28:18 +0300

    Added response getting from mail. For now only works with IMAP and looks for word reply and mail ID in subject.

---
+++ pjutils/get_mail.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os, sys, imaplib, rfc822
+import string, re, StringIO
+
+class GetMail():
+
+    def get_imap(self, server_info, msg_id):
+        M = imaplib.IMAP4_SSL(server_info['server'])
+        M.login(server_info['username'], server_info['password'])
+        M.select()
+        messages = []
+        typ, data = M.search(None, 'SUBJECT', 'reply%s' % msg_id)
+        for num in data[0].split():
+            typ, data = M.fetch(num, '(RFC822)')
+
+            file = StringIO.StringIO(data[0][1])
+            message = rfc822.Message(file)
+
+#            print message.fp.read()
+            messages.append(message.fp.read())
+
+        M.close()
+        M.logout()
+        return messages
+
+    def get_pop3(self, server_info, msg_id):
+        print 'Not yet implemented'
+        return []
+
+    def get_mail(self, server_info, msg_id):
+        messages = []
+        if server_info['type'] == 'IMAP':
+            messages = self.get_imap(server_info, msg_id)
+        elif server_info['type'] == 'POP3':
+            messages = self.get_pop3(server_info, msg_id)
+        return messages
+
