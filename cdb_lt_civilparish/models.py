9bef6605130ea5b9ac733053d8b8969b39399960
Darius Damalakas <darius.damalakas@intelliflo.com>
2010-10-20 13:57:13 +0300

    imported civil parish addresses :)

--- cdb_lt_civilparish/models.py
+++ cdb_lt_civilparish/models.py
@@ -4,7 +4,7 @@
 from django.db import models
 
 # Create your models here.
-from contactdb.models import PersonNameField, InstitutionNameField, PhoneField
+from contactdb.models import PersonNameField, InstitutionNameField, PhoneField, AddressNameField
 
 class CivilParish(models.Model):
     name = InstitutionNameField()
@@ -23,3 +23,11 @@ class CivilParishMember(models.Model):
     officeAddress = models.CharField(max_length = 100)
     uniqueKey = models.IntegerField()
 
+
+class CivilParishStreet(models.Model):
+    """ Represents a mapping between a constituency, a district and a street in Lithuania
+    """
+    civilParish = models.ForeignKey(CivilParish)
+    municipality = AddressNameField()
+    street = AddressNameField(db_index = True)
+    city = AddressNameField(db_index = True)
