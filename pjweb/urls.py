504fa6aaecf66892d416f1445ee6d4b3d78add3f
dariusdamalakas <dariusdamalakas@localhost>
2010-10-28 20:07:00 +0300

    Adding comments for urls.py Removing unused code from views.py Fixing index.html template, adding object id

--- pjweb/urls.py
+++ pjweb/urls.py
@@ -14,16 +14,30 @@ urlpatterns = patterns('parasykjiems.pjweb.views',
     # Uncomment the admin/doc line below and add 'django.contrib.admindocs'
     # to INSTALLED_APPS to enable admin documentation:
     # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Handle start page
     (r'^$', 'index'),
     (r'^contact/(\w+)/(\d+)/no_email/$', 'no_email'),
     (r'^contact/(\w+)/(\w+)/(\w+)/smtp_error/$', 'smtp_error'),
-    #(r'^contact/(\w+)/(\d+)/select_privacy/$', 'select_privacy'),
+
+    (r'^chose_rep/(\w+/)$', 'chose_representative'),
+
+    # user is presented with a list of representatives whom he can write to
     (r'^(\d+)/$', 'constituency'),
+
+    # contact a representative: write an email
     (r'^contact/(\w+)/(\d+)/$', 'contact'),
+
+    # display a list of all public emails
     (r'^public_mails/$', 'public_mails'),
+
+    # displays a single email. this email was sent via our site, and was marked as public
     (r'^public/(\d+)/$', 'public'),
+
+    # feedback page
     (r'^feedback/$', 'feedback'),
-#    (r'^feedback/thanks/$', 'thanks'),
+
+    # about page
     (r'^about/$', 'about'),
     (r'^response/(\d+)/(\d+)/$', 'response'),
     (r'^i18n/', include('django.conf.urls.i18n')),
