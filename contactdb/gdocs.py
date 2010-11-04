df3f16d3e8396e22b21ba0488e5c4758147eff41
dariusdamalakas <dariusdamalakas@localhost>
2010-11-04 18:38:37 +0200

    Creating function to upload data to google docs

--- contactdb/gdocs.py
+++ contactdb/gdocs.py
@@ -1,3 +1,5 @@
+import logging
+
 try:
   from xml.etree import ElementTree
 except ImportError:
@@ -10,8 +12,27 @@ import atom
 import getopt
 import sys
 import string
+logger = logging.getLogger(__name__)
 
 
+class SpreadSheetUpdater:
+    def __init__(self, spreadSheetClient):
+        self.spreadSheetClient = spreadSheetClient
+        self.gd_client = self.spreadSheetClient.gd_client
+        self.feed = self.spreadSheetClient.GetListFeed()
+
+    def AppendRow(self, row_data):
+        entry = self.gd_client.InsertRow(row_data, self.spreadSheetClient.curr_key, self.spreadSheetClient.curr_wksht_id)
+        if isinstance(entry, gdata.spreadsheet.SpreadsheetsList) == False:
+            logger.error('Insert failed')
+
+    def UpdateRow(self, i, row_data):
+        entry = self.gd_client.UpdateRow(self.feed.entry[i], row_data)
+        if isinstance(entry, gdata.spreadsheet.SpreadsheetsList) == False:
+            logger.error('Update failed')
+
+    def DeleteRow(self, i):
+        self.gd_client.DeleteRow(self.feed.entry[i])
 
 
 class SpreadSheetClient:
@@ -58,9 +79,48 @@ class SpreadSheetClient:
         feed = self.gd_client.GetWorksheetsFeed(self.curr_key)
         id_parts = feed.entry[worksheetNumber].id.text.split('/')
         self.curr_wksht_id = id_parts[len(id_parts) - 1]
+        self._CreateListFeed()
+
+    def DeleteAllRows(self):
+        count = 0
+        for i, entry in enumerate(self.listFeed.entry):
+            count += 1
+        logger.info("will delete %s rows" % count)
+        for i in range(0, count):
+            self.gd_client.DeleteRow(listFeed.entry[i])
+            logger.debug("deletig %s / %s" % (i, count))
+        logger.info("deleted")
+
+    def GetRowsCount(self):
+        count = 0
+        for i, entry in enumerate(self.listFeed.entry):
+            count += 1
+        return count
+
+
+    def ToDictionaryFromRow(self, googleRow):
+        """ builds a dictionary, and encodes values with utf-8 from a google row.
+        See GetAllRows() for details. You can feed results from GetAllRows
+        directly to this function"""
+        d = dict([(k, v.text) for k, v in googleRow.iteritems()])
+
+        for k in d.iterkeys():
+            if d[k] is None:
+                continue
+            d[k] = d[k].replace("\n", " ")
+
+            d[k] = d[k].encode("utf-8")
+        return d
+
+    def GetListFeed(self):
+        return self.listFeed
+
+    def _CreateListFeed(self):
+        self.listFeed = self.gd_client.GetListFeed(self.curr_key, self.curr_wksht_id)
+        return self.listFeed
+
 
     def GetAllRows(self):
         """ returns a a generator which yields a dictionary of values for each row"""
-        listFeed = self.gd_client.GetListFeed(self.curr_key, self.curr_wksht_id)
-        for i, entry in enumerate(listFeed.entry):
+        for i, entry in enumerate(self.listFeed.entry):
             yield entry.custom
--- contactdb/management/commands/downloadDocs.py
+++ contactdb/management/commands/downloadDocs.py
@@ -1,11 +1,90 @@
 from django.core.management.base import BaseCommand
 from django.db import transaction
-from contactdb.gdocs import SpreadSheetClient
+from contactdb.gdocs import SpreadSheetClient, SpreadSheetUpdater
 from settings import *
 import csv
 import pjutils.uniconsole
 from contactdb.imp import GoogleDocsSources, ImportSources
 import os
+from cdb_lt_streets.management.commands.ltGeoDataImportCsv import ltGeoDataSources
+logger = logging.getLogger(__name__)
+
+class SpreadSheetUploader:
+
+    def __init__(self, docName, fileName):
+        logger.debug("logging in to GDocs")
+        self.client = SpreadSheetClient(GlobalSettings.GOOGLE_DOCS_USER, GlobalSettings.GOOGLE_DOCS_PASSWORD)
+        self.client.SelectSpreadsheet(docName)
+        self.client.SelectWorksheet(0)
+        logger.debug("logged into GDocs")
+
+        logger.debug("reading all csv file")
+        self.dictReader = csv.DictReader(open(fileName, "rt"), delimiter = "\t")
+        self.dictReader = list(self.dictReader)
+        logger.debug("read all csv file")
+
+        self.UploadDocument()
+
+    def dictionariesEqual(self, dict1, dict2):
+        for key1 in dict1.iterkeys():
+            val1 = dict1[key1]
+            if (dict2.has_key(key1) == False):
+                if val1 is None:
+                    continue
+                if val1.strip() == u"":
+                    continue
+            val2 = dict2[key1]
+            if (val1 is None):
+                val1 = ""
+            val1 = val1.strip()
+
+            if (val2 is None):
+                val2 = ""
+            val2 = val2.strip()
+            if (val1 != val2):
+                return False
+        return True
+
+
+
+    def UploadDocument(self):
+        logger.debug("preparing to update document")
+        csvCount = len(self.dictReader)
+        gdocRows = [self.client.ToDictionaryFromRow(r) for r in self.client.GetAllRows()]
+        gdocCount = len(gdocRows)
+
+        logger.debug("pre-fetched all rows")
+        updater = SpreadSheetUpdater(self.client)
+
+        maxx = max(csvCount, gdocCount)
+        i = - 1
+        logger.debug("starting to update")
+        for gdocValues in gdocRows:
+            i += 1
+            csvValues = self.dictReader[i]
+
+            if (self.dictionariesEqual(gdocValues, csvValues) == True):
+                logger.debug("Not changed %s / %s" % (i, maxx))
+                continue
+
+            logger.debug("Uploading row %s / %s" % (i, maxx))
+            updater.UpdateRow(i, self.dictReader[i])
+
+        for i in range(i, maxx):
+            logger.debug("Uploading row %s / %s" % (i, maxx))
+            if i >= csvCount:
+                updater.DeleteRow(i)
+            elif i >= gdocCount:
+                updater.AppendRow(self.dictReader[i])
+            else:
+                updater.UpdateRow(i, self.dictReader[i])
+
+
+
+
+
+
+
 
 
 """ Downloads a google doc, and saves it to a file as csv file.
@@ -25,18 +104,6 @@ class GoogleDocDownloader:
 
         return writer
 
-    def buildValuesDict(self, googleRow):
-        """ builds a dictionary, and encodes values with utf-8"""
-        d = dict([(k, v.text) for k, v in googleRow.iteritems()])
-
-        for k in d.iterkeys():
-            if d[k] is None:
-                continue
-            d[k] = d[k].replace("\n", " ")
-
-            d[k] = d[k].encode("utf-8")
-        return d
-
     def downloadDoc(self, docName, fileName):
         #client.SelectSpreadsheet("ParasykJiems.lt public contact db")
         print "downloading  '%s' to '%s'" % (docName, fileName)
@@ -50,18 +117,25 @@ class GoogleDocDownloader:
         writer = None
         for row in self.client.GetAllRows():
             # row is a custom object, so lets construct a normal dictionary from it with keys and values
-            val = self.buildValuesDict(row)
+            val = self.client.ToDictionaryFromRow(row)
             if (writer is None):
                 writer = self.openWriter(fileName, val)
             writer.writerow(val)
         print "ok"
 
+        self.client.GetAllRows()
 
 
-""" Uses GoogleDocDownloader inside to actually download a google doc and save it to file
-as csv"""
+
+
+
 def downloadDoc(docName, fileName):
+    """ Uses GoogleDocDownloader inside to actually download a google doc and save it to file
+    as csv"""
     GoogleDocDownloader().downloadDoc(docName, fileName)
+
+
+
 
 class Command(BaseCommand):
     args = '<>'
@@ -74,6 +148,13 @@ class Command(BaseCommand):
         """ Downloads documents as csv (tab-delimited) files from google docs"""
         allRecords = os.getcwd()
 
+
+
+
+
+        #docName = ltGeoDataSources.commonIndexes[0]
+        #SpreadSheetUploader(docName[0], docName[1])
+        downloadDoc(GoogleDocsSources.LithuanianMPs, ImportSources.LithuanianMPs)
         downloadDoc(GoogleDocsSources.LithuanianMPs, ImportSources.LithuanianMPs)
         downloadDoc(GoogleDocsSources.LithuanianCivilParishMembers, ImportSources.LithuanianCivilParishMembers)
         downloadDoc(GoogleDocsSources.LithuanianMunicipalityMembers, ImportSources.LithuanianMunicipalityMembers)
