7032f0e0828a79b3b448d56441803b8910d493ff
Darius Damalakas <darius.damalakas@intelliflo.com>
2010-11-05 14:17:58 +0200

    Adding support for ranges for commands

--- cdb_lt_streets/management/commands/ltGeoDataUploadDocs.py
+++ cdb_lt_streets/management/commands/ltGeoDataUploadDocs.py
@@ -3,7 +3,7 @@ from contactdb.management.commands.downloadDocs import GoogleDocUploader
 from cdb_lt_streets.management.commands.ltGeoDataImportCsv import ltGeoDataSources
 from contactdb.gdocs import GoogleDocsLogin, GoogleDocsDocument
 from settings import *
-from cdb_lt_streets.management.commands.ltGeoDataCrawl import RegisterCenterPageLocations
+from cdb_lt_streets.management.commands.ltGeoDataCrawl import RegisterCenterPageLocations, ExtractRange
 from pydoc import doc
 logger = logging.getLogger(__name__)
 
@@ -23,9 +23,16 @@ class Command(BaseCommand):
         #GoogleDocUploader(docName[0], docName[1])
 
         l = zip(ltGeoDataSources.LithuanianStreetIndexes, RegisterCenterPageLocations.AllData)
+
+        if (len(args) >= 1):
+            fromNumber, toNumber = ExtractRange(args[0])
+        if (toNumber is None):
+            toNumber = len(l)
+
+
         login = GoogleDocsLogin(GlobalSettings.GOOGLE_DOCS_USER, GlobalSettings.GOOGLE_DOCS_PASSWORD)
 
-        for street, regCenterPage in l:
+        for street, regCenterPage in l[fromNumber : toNumber]:
             file = regCenterPage[1]
             if (os.path.exists(file) == False):
                 continue
