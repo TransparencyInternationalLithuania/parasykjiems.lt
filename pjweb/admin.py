360a8f50bd71b8947760499fc06d08a5e87b86a1
kroitus <928358+kroitus@users.noreply.github.com>
2010-11-12 00:36:37 +0200

    Edited emails admin pages - added ability to filter emails and history

--- pjweb/admin.py
+++ pjweb/admin.py
@@ -9,14 +9,20 @@ class MsgAdmin(admin.ModelAdmin):
         (_('Message'),            {'fields': ['message', 'msg_state', 'public']}),
     ]
     list_display = ('sender_name', 'msg_state', 'mail_date', 'public')
+    list_filter = ['mail_date', 'sender_mail', 'recipient_mail']
+    search_fields = ['recipient_name', 'sender_name', 'recipient_mail', 'sender_mail']
+    date_hierarchy = 'mail_date'
     
 admin.site.register(Email, MsgAdmin)
 
 class MsgHistAdmin(admin.ModelAdmin):
     fieldsets = [
         (_('Recipients'), {'fields': ['recipient', 'sender']}),
-        (_('States'),            {'fields': ['mail', 'mail_state']}),
+        (_('States'),     {'fields': ['mail', 'mail_state']}),
     ]
     list_display = ('sender', 'recipient', 'mail_state', 'request_date')
+    list_filter = ['request_date', 'sender', 'recipient']
+    search_fields = ['recipient', 'sender']
+    date_hierarchy = 'request_date'
 
 admin.site.register(MailHistory, MsgHistAdmin)
