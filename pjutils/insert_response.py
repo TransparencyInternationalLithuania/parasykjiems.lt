22c5aac6aacd90af07c53cf19e0e8a5634008864
Darius Damalakas <darius.damalakas@gmail.com>
2011-01-23 17:27:03 +0200

    removing unused code

--- pjutils/insert_response.py
+++ pjutils/insert_response.py
@@ -49,63 +49,6 @@ class InsertResponse():
 
         return receiver[0]
 
-    """
-    def insert_response(self, mail_id):
-        getmail = GetMail()
-        resp = False
-        if GlobalSettings.MAIL_SERVER:
-            server_info = {
-                'server':GlobalSettings.MAIL_SERVER,
-                'port':GlobalSettings.MAIL_PORT,
-                'username':GlobalSettings.MAIL_USERNAME,
-                'password':GlobalSettings.MAIL_PASSWORD,
-                'type':GlobalSettings.MAIL_SERVER_TYPE
-            }
-            mail = Email.objects.get(id=mail_id)
-            responses = getmail.get_mail(server_info, mail_id, mail.response_hash)
-            responder = self.get_rep(mail.recipient_id, mail.recipient_type)
-            for response in responses:
-                try:
-                    message = unicode(response,'UTF-8')
-                except:
-                    message = unicode(response,'iso-8859-13')
-                
-                resp = Email(
-                    sender_name = mail.recipient_name,
-                    sender_mail = responder.email,
-                    recipient_id = mail.recipient_id,
-                    recipient_type = mail.recipient_type,
-                    recipient_name = mail.sender_name,
-                    recipient_mail = mail.sender_mail,
-                    msg_type = 'Response',
-                    response_hash = mail.response_hash,
-                    answer_to = mail.id,
-                    public = mail.public,
-                )
-                # if previous email was public, then we save the reply message text to db
-                # else we delete it, and simply send whole email straight to the person
-                # who asked the question in the first place
-                if mail.public:
-                    resp.message = message
-                else:
-                    resp.message = ''
-                resp.save()
-
-                # send a mail message to original person who asked a question.
-                # Reply-to will not be an exact recipients email (resp.sender_mail),
-                # but a "no_reply" address, meaning that we do not support "discussion" style
-                # responses now.
-                email = EmailMessage(u'Gavote atsakymą nuo %s' % resp.sender_name, response, settings.EMAIL_HOST_USER,
-                    [resp.recipient_mail], [],
-                    headers = {'Reply-To': 'no_reply_parasykjiems@gmail.com'})
-                email.send()
-
-            if resp:
-                return resp
-            else:
-                return False
-        return False"""
-
     def insert_resp(self, mail_info):
         mail = None
         try:
