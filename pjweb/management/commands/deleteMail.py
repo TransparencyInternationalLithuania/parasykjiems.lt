a9716f6f6eca3355c44877b69f12d01bdf186d0e
Darius Damalakas <darius.damalakas@gmail.com>
2011-01-31 13:27:42 +0200

    more ways to delete imap email

--- pjweb/management/commands/deleteMail.py
+++ pjweb/management/commands/deleteMail.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
+import time
 from django.core.management.base import BaseCommand
 from cdb_lt_streets.management.commands.ltGeoDataCrawl import ExtractRange
 from pjutils.get_mail import GetMail
@@ -22,19 +22,44 @@ class Command(BaseCommand):
             'type':GlobalSettings.mail.IMAP.EMAIL_HOST_TYPE
             }
 
-        fromNumber, toNumber = ExtractRange(args[0])
-
         getmail = GetMail()
         getmail.login(server_info)
         insert = InsertResponse()
         mailNumbers = getmail.getUnseenMessages() + getmail.getSeenMessages()
 
+        print "there are these emails: %s" % mailNumbers
+        if len(args) < 1:
+            print "Use this command to delete emails. Specify emails by comman (no spaces), or as a python range"
+            return
 
-        for num in mailNumbers:
-            numInt = int(num)
-            if numInt >= fromNumber and numInt < toNumber:
-                print "deleting mail %s" % num
-                getmail.markMessageForDeletion(num)
+        emailsToDelete = []
+
+        if args[0].find(':') >= 0:
+            fromNumber, toNumber = ExtractRange(args[0])
+            for num in mailNumbers:
+                numInt = int(num)
+                if numInt >= fromNumber and numInt < toNumber:
+                    emailsToDelete.append(numInt)
+        elif args[0].find(',') >= 0:
+            for p in args[0].split(','):
+                emailsToDelete.append(int(p))
+        else:
+            try:
+                emailsToDelete.append(int(args[0]))
+            except ValueError:
+                print "incorrect argument passed %s" % args[0]
+                return
+
+
+
+        print "will delete emails %s" %emailsToDelete
+        seconds = 5
+        print "Is that ok? Will wait for %s seconds" % seconds
+        time.sleep(seconds)
+        
+        for num in emailsToDelete:
+            print "deleting mail %s" % num
+            getmail.markMessageForDeletion(num)
 
 
 
