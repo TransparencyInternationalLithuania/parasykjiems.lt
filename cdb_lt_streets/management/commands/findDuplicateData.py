1c976988b25039eae206f26095027709649352ce
Darius Damalakas <darius.damalakas@intelliflo.com>
2011-02-05 15:42:36 +0200

    Creating findDuplicateData command for civil praish

--- cdb_lt_streets/management/commands/findDuplicateData.py
+++ cdb_lt_streets/management/commands/findDuplicateData.py
@@ -1,48 +1,52 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
+from cdb_lt_civilparish.models import CivilParishStreet
+import logging
 from cdb_lt_seniunaitija.models import SeniunaitijaStreet
 from cdb_lt_streets.management.commands.ltGeoDataCrawl import ExtractRange
-from cdb_lt_streets.streetUtils import getCityNominative
+from cdb_lt_streets.streetUtils import getCityNominative, cityNameIsGenitive, getCityGenitive
 from django.core.management.base import BaseCommand
 from pjutils.timemeasurement import TimeMeasurer
-from pjweb.views import findSeniunaitijaMembers
+from pjweb.views import findSeniunaitijaMembers, findCivilParishMembers
 from django.db import transaction
+logger = logging.getLogger(__name__)
 
 class Command(BaseCommand):
     args = '<>'
     help = ''
 
-    @transaction.commit_on_success
-    def handle(self, *args, **options):
-
+    def findGeneric(self, streetDataToLooop, functionToCall):
         totalNumberOfStreets = 0
 
         fromNumber = 0
         toNumber = None
-        if len(args) >= 1:
-            fromNumber, toNumber = ExtractRange(args[0])
+        if len(self.args) >= 1:
+            fromNumber, toNumber = ExtractRange(self.args[0])
 
 
         if toNumber is not None:
             print "will test streets from %s to %s" % (fromNumber, toNumber)
-            allStreets = SeniunaitijaStreet.objects.all()
+            allStreets = streetDataToLooop.objects.all()
             allStreets = list(allStreets[fromNumber: toNumber])
         else:
             print "will test all streets"
-            allStreets = SeniunaitijaStreet.objects.all()
+            allStreets = streetDataToLooop.objects.all()
             allStreets = list(allStreets)
 
-        start = TimeMeasurer()
+
         for streetObject in allStreets:
-            print "row %s" % (totalNumberOfStreets + fromNumber)
             totalNumberOfStreets += 1
             municipality = streetObject.municipality
             city = streetObject.city
             house_number = streetObject.numberFrom
-            city_genitive = city
-            street = streetObject.street
-            city = getCityNominative(municipality=municipality, city_genitive=city_genitive, street= street)
+
+            street= streetObject.street
+            if cityNameIsGenitive(municipality=municipality, city_genitive=city, street= street):
+                city_genitive = city
+                city = getCityNominative(municipality=municipality, city_genitive=city_genitive, street= street)
+            else:
+                city = city
+                city_genitive = getCityGenitive(municipality=municipality, city=city, street= street)
 
 
             """municipality = u"Šilalės rajono"
@@ -52,16 +56,18 @@ class Command(BaseCommand):
             city = None"""
 
             additionalKeys = {"city_genitive" : city_genitive}
-            members = findSeniunaitijaMembers(municipality, city, street, house_number, **additionalKeys)
+            members = functionToCall(municipality, city, street, house_number, **additionalKeys)
 
             if house_number is None:
                 house_number = ""
             if street is None:
                 street = ""
 
+            logger.info("row: %s" % (totalNumberOfStreets + fromNumber ))
             total = len(members)
             if total == 1:
                 continue
+            print "row %s" % (totalNumberOfStreets + fromNumber)
             print "adress: %s %s %s %s" % (street, house_number, city_genitive, municipality)
             print "%s member" % total
 
@@ -69,10 +75,18 @@ class Command(BaseCommand):
                 print "%s %s" % (m.name, m.surname)
 
             print ""
-
-        seconds = start.ElapsedSeconds()
+        seconds = self.start.ElapsedSeconds()
         rate = str(totalNumberOfStreets / seconds)
         print "checking at %s rows per second (total sec: %d, rows: %d)" % (rate, seconds, totalNumberOfStreets)
 
-        print u"total spent time %d seconds" % (start.ElapsedSeconds())
+
+    @transaction.commit_on_success
+    def handle(self, *args, **options):
+
+        self.args = args
+        self.start = TimeMeasurer()
+        #self.findGeneric(SeniunaitijaStreet, findSeniunaitijaMembers)
+        self.findGeneric(CivilParishStreet, findCivilParishMembers)
+
+        print u"total spent time %d seconds" % (self.start.ElapsedSeconds())
         
