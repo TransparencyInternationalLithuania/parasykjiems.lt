509ed6cc7a5e58ccb3df8b4a83fc632a82529009
Darius Damalakas <darius.damalakas@intelliflo.com>
2011-02-13 22:04:52 +0200

    Did not correctly search representatives if numberTo was None

--- cdb_lt_streets/tests/__init__.py
+++ cdb_lt_streets/tests/__init__.py
@@ -1,5 +1,5 @@
 from __future__ import absolute_import
-from cdb_lt_streets.tests.InstitutionIndexes.TestInstitutionIndexes import TestSearchCivilParishStreets_SingleStreet, TestSearchInstitutionStreets_WithStreetAndHouseNumber, TestSearchInstitutionStreets_SingleRepresentative, TestSearchInstitutionStreets_ArminuKaimas
+from cdb_lt_streets.tests.InstitutionIndexes.TestInstitutionIndexes import TestSearchCivilParishStreets_SingleStreet, TestSearchInstitutionStreets_WithStreetAndHouseNumber, TestSearchInstitutionStreets_SingleRepresentative, TestSearchInstitutionStreets_ArminuKaimas, TestSearchInstitutionStreets_NumberToIsNone
 from cdb_lt_streets.tests.TestAddressDeducer import TestAddressDeducer
 from cdb_lt_streets.tests.TestHouseNumberUtils import TestContainsHouseNumbers
 from cdb_lt_streets.tests.TestLTRegisterCenter import TestLTRegisterCenterLocations, TestLTRegisterCenterLinks, TestLTRegisterCenterOtherLinks
@@ -19,5 +19,6 @@ __test__ = {
     'TestSearchInstitutionStreets_SingleRepresentative': TestSearchInstitutionStreets_SingleRepresentative,
     'TestSearchLtStreetIndex_SingleStreet' : TestSearchLtStreetIndex_SingleStreet,
     'TestSearchLtStreetIndex_StreetsWithNumbersInName': TestSearchLtStreetIndex_StreetsWithNumbersInName,
-    'TestSearchInstitutionStreets_ArminuKaimas' : TestSearchInstitutionStreets_ArminuKaimas
+    'TestSearchInstitutionStreets_ArminuKaimas' : TestSearchInstitutionStreets_ArminuKaimas,
+    'TestSearchInstitutionStreets_NumberToIsNone' : TestSearchInstitutionStreets_NumberToIsNone
 }
