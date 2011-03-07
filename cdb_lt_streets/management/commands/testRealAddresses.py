01afc34061ad1d99fd3872ebe9b70f6dc665b09b
Darius Damalakas <darius.damalakas@gmail.com>
2011-03-07 16:39:00 +0200

    printing percentages for incorrect results for command testRealAddresses

--- cdb_lt_streets/management/commands/testRealAddresses.py
+++ cdb_lt_streets/management/commands/testRealAddresses.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import csv
+import sys
 
 from django.core.management.base import BaseCommand
 from django.core import management
@@ -9,68 +10,20 @@ from cdb_lt_streets.searchInIndex import deduceAddress, searchInIndex
 from cdb_lt_streets.searchMembers import findMPs, findMunicipalityMembers, findCivilParishMembers, findSeniunaitijaMembers
 import os
 from pjutils.timemeasurement import TimeMeasurer
+from settings import GlobalSettings
 
 class Command(BaseCommand):
     args = '<>'
     help = """Searches for members against hand-written list of addresses and checks whether exactly 1 representative is returned"""
 
-
-    def getDefaultAddresses(self):
-        return [u"Palemono 74",
-                    u"žygio g., Vilnius",
-                    u"sodų g. vilnius",
-                    u"šaulių 23a, klaipėda",
-                    u"J. Žemgulio gatvė 10 Kauno miestas Kauno miesto savivaldybė"]
-
-
-    def handle(self, *args, **options):
-        """ Not very usefull command, until we know what result exactly we want. Now will print lots of information when running.
-        Will search for every address in addresses list and check if exactly one member is found"""
-
-        exactQueries = []
-        addresses = []
-        self.start = TimeMeasurer()
-
-        fromNumber = 0
-        toNumber = None
-        if len(args) >= 2:
-            fromNumber, toNumber = ExtractRange(args[1])
-
-        if len(args) > 0:
-            file = args[0]
-            curDir = os.getcwd()
-
-            file = os.path.join(curDir, file)
-            f = open(file, mode="r")
-            addresses = f.readlines()
-            addresses = [unicode(val.strip(), 'utf-8') for val in addresses]
-
-            exactQueries = []
-        else:
-            addresses = self.getDefaultAddresses()
-
-            exactQueries = [{'municipality':u"Šilutės rajono savivaldybė",
-                              'civilParish':u"Šilutės seniūnija",
-                              'city':u"Kalininkų kaimas",
-                              'street':u"",
-                              'house_number':u""}
-            ]
-
-
+    def testAddresses(self, addresses):
         functions = [findMPs,
                      findMunicipalityMembers,
                      findCivilParishMembers,
                      findSeniunaitijaMembers]
 
-        """missingDataByType = {"MP": [],
-                     "Mayor": [],
-                     "CivilParish": [],
-                     "Seniunaitija": []}
-"""
         missingDataByStreet = {}
 
-
-        addresses = addresses[fromNumber: toNumber]
         count = 0
         total = len(addresses)
         for address in addresses:
@@ -82,7 +35,6 @@ class Command(BaseCommand):
                 continue
 
             count += 1
-            #print "%s" % address
 
             if missingDataByStreet.has_key(address):
                 continue
@@ -97,9 +49,6 @@ class Command(BaseCommand):
 
             missingDataByStreet.setdefault(address, [address, len(found_entries)])
             if len(found_entries) != 1:
-                #missingDataByStreet[address] = [address, u"", u"", u"", len(found_entries)]
-                #print "Found more than one entry for address %s, %s " % (count + fromNumber, address)
-                #print found_entries
                 continue
 
             f = found_entries[0]
@@ -118,63 +67,72 @@ class Command(BaseCommand):
             for function in functions:
                 members = function(**additionalKeys)
                 missingDataByStreet[address].append(len(members))
-                """if len(members) != 1:
-                    queue = missingDataByType[key]
-                    queue.append([address, members])"""
+                
+        """print u"total spent time %d seconds" % (self.start.ElapsedSeconds())
+        if count == 0:
+            print "Total rows %s, %s seconds per row" % (count, 0)
+        else:
+            print "Total rows %s, %s seconds per row" % (count, self.start.ElapsedSeconds() / float(count))"""
+        return missingDataByStreet
+        
 
 
+    def readAddresses(self, addressFile):
+        curDir = os.getcwd()
+
+        file = os.path.join(curDir, addressFile)
+        f = open(file, mode="r")
+        addresses = f.readlines()
+        addresses = [unicode(val.strip(), 'utf-8') for val in addresses]
+        return addresses
+
+    def printPercentages(self, missingDataByStreet, allAddresses):
+        totalStreets = len(allAddresses)
+
+        incorrectData = [0, 0, 0, 0, 0, 0]
+
+        for address, values in missingDataByStreet.iteritems():
+            for i in range(1, len(values)):
+                count = values[i]
+                if count != 1:
+                    incorrectData[i] += 1
+
+        print u"Iš viso gatvių: %s" % totalStreets
+        labels = [u"Gatvių", u"Seimo narių", u"Merų", u"Seniūnų", u"Seniūnaičių"]
+        for i in range(0, len(labels)):
+            print "%s %s: %s%%" % (labels[i], incorrectData[i + 1], incorrectData[i + 1] / float(totalStreets) * 100)
+        print "\n\n"
 
 
-        """
-        for keys in exactQueries:
-            for key, function in functions.iteritems():
-                members = function(**keys)
-                if len(members) != 1:
-                    queue = missingDataByType[key]
-                    addr = u"%s %s %s" % (keys['municipality'], keys['civilParish'], keys['city'])
-                    queue.append([addr, members])
+        
 
-                    missingDataByStreet.setdefault(address, [])
-                    missingDataByStreet[address].append(len(members))
-        """
+    def handle(self, *args, **options):
+        """ Not very usefull command, until we know what result exactly we want. Now will print lots of information when running.
+        Will search for every address in addresses list and check if exactly one member is found"""
 
+        self.start = TimeMeasurer()
 
+        fromNumber = 0
+        toNumber = None
+        if len(args) >= 2:
+            fromNumber, toNumber = ExtractRange(args[1])
 
+        if len(args) > 0:
+            addresses = self.readAddresses(args[0])
+        else:
+            print "Please specify file with addresses"
+            return
+
+        addresses = addresses[fromNumber: toNumber]
+        missingDataByStreet = self.testAddresses(addresses)
 
         print "\n\n"
-        """# print streets and percentages
-        for type, queue in missingDataByType.iteritems():
-            print "\n"
-            print "missing data for %s" % type
 
-            for item in queue:
-                street, members = item
-                print "%s \t: %s members" % (street.rjust(25, ' '), len(members))
-            print "total rows: %s " % count
-            print "total incorrect results: %s" % len(queue)
-            if len(queue) == 0:
-                print "percentage %s%%" % (0.00)
-            else:
-                print "percentage %s%%" % (float(len(queue))/ count * 100)
-
-
-        # just print percentages
-        print "\n\n\n"
-        for type, queue in missingDataByType.iteritems():
-            print "\n"
-            print "missing data for %s" % type
-            print "total rows: %s " % count
-            print "total incorrect results: %s" % len(queue)
-            if len(queue) == 0:
-                print "percentage %s%%" % (0.00)
-            else:
-                print "percentage %s%%" % (float(len(queue))/ count * 100)
-"""
+        self.printPercentages(missingDataByStreet, addresses)
 
 
         # print as csv style
-        f = open(u"testRealAddresses.csv", "w")
-        writer = csv.writer(f)
+        writer = csv.writer(sys.stdout)
         header = [u'Adresas', u'Surasta adresų', u'Seimo narys', u'Meras', u'Seniūnas', u'Seniūnaitis']
         header = [v.encode('utf-8') for v in header]
         writer.writerow(header)
@@ -182,12 +140,3 @@ class Command(BaseCommand):
             values = ["%s" % v for v in values]
             values = [v.encode('utf-8') for v in values ]
             writer.writerow(values)
-        f.close()
-
-
-        print "\n\n\n"
-        print u"total spent time %d seconds" % (self.start.ElapsedSeconds())
-        if count == 0:
-            print "Total rows %s, %s seconds per row" % (count, 0)
-        else:
-            print "Total rows %s, %s seconds per row" % (count, self.start.ElapsedSeconds() / float(count))
