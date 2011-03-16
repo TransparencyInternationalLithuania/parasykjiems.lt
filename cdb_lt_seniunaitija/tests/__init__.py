35d023b26bf8b8c1080b54e39f89c81b39633b8c
Darius Damalakas <darius.damalakas@gmail.com>
2011-03-16 16:27:13 +0200

    moving other tests

--- cdb_lt_seniunaitija/tests/__init__.py
+++
@@ -1,7 +0,0 @@
-from __future__ import absolute_import
-from cdb_lt_seniunaitija.tests.TestSeniunaitijaAddressExpander import TestSeniunaitijaAddressExpander
-
-__test__ = {
-    'TestSeniunaitijaAddressExpander' : TestSeniunaitijaAddressExpander
-}
-
