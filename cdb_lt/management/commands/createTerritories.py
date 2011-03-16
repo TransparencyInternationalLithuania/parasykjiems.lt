1e83a9b13b9fe6e5ddae7a6b82b0a14f87ea480b
Darius Damalakas <darius.damalakas@gmail.com>
2011-03-16 15:34:27 +0200

    Adding Vilnius parishes

--- cdb_lt/management/commands/createTerritories.py
+++ cdb_lt/management/commands/createTerritories.py
@@ -4,7 +4,9 @@
 import os
 from django.core.management.base import BaseCommand
 from cdb_lt.civilParishStreetReader import civilParishStreetReader
+from cdb_lt.civilParishVilniusStreetReader import civilParishVilniusStreetReader
 from cdb_lt.management.commands.createMembers import makeCivilParishInstitutionName, cityNameGetterGenitive
+from cdb_lt.management.commands.importSources import ltGeoDataSources_Institution
 from cdb_lt.mpStreetReader import mpStreetReader
 from cdb_lt.municipalityStreetReader import municipalityStreetReader
 from cdb_lt.seniunaitijaTerritoryReader import seniunaitijaStreetReader
@@ -20,17 +22,21 @@ class Command(BaseCommand):
         # create Country data
         fileNames = [f[1] for f in ltGeoDataSources_Country.LithuanianAddresses]
         importCountryData(csvFileNames=fileNames)
-
+"""
         # create addresses for civilParish
         fileNames = [f[1] for f in ltGeoDataSources_Institution.civilParishAddresses]
-        importInstitutionTerritoryYielder(addressYielder=civilParishStreetReader(csvFileNames=fileNames, institutionNameGetter=makeCivilParishInstitutionName, cityNameGetter = cityNameGetterGenitive), , institutionCode = "civpar")
+        #importInstitutionTerritoryYielder(addressYielder=civilParishStreetReader(csvFileNames=fileNames, institutionNameGetter=makeCivilParishInstitutionName, cityNameGetter = cityNameGetterGenitive), , institutionCode = "civpar")
 
+        fileNames = [f[1] for f in ltGeoDataSources_Institution.civilParishAddresses]
+        importInstitutionTerritoryYielder(addressYielder=civilParishVilniusStreetReader(), institutionCode = "civpar")
+
+        """
         # create addresses for seniunaitija
         importInstitutionTerritoryYielder(addressYielder=seniunaitijaStreetReader(), institutionCode = "seniunaitija")
 
-"""
         # create Municipality data
-        #importInstitutionTerritoryYielder(addressYielder=municipalityStreetReader(), institutionCode = "mayor")
+        importInstitutionTerritoryYielder(addressYielder=municipalityStreetReader(), institutionCode = "mayor")
 
         # create MP data
         importInstitutionTerritoryYielder(addressYielder=mpStreetReader(), institutionCode = "mp")
+                """
