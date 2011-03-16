cd71155f7a218ee225a0be471276279d9552952d
Darius Damalakas <darius.damalakas@intelliflo.com>
2011-03-16 16:27:13 +0200

    moving other tests

--- cdb_lt/tests/__init__.py
+++ cdb_lt/tests/__init__.py
@@ -1,10 +1,12 @@
 from __future__ import absolute_import
 from cdb_lt.tests.mpStreetReaderTests.TestImportLithuanianCounties import TestImportLithuanianCounties
 from cdb_lt.tests.mpStreetReaderTests.TestParseConstituencies import TestPollingDistrictStreetExpander, TestLithuanianConstituencyParser, TestAddressParser
+from cdb_lt.tests.seniunaitijaTerritoryReaderTests.TestSeniunaitijaAddressExpander import TestSeniunaitijaAddressExpander
 
 __test__ = {
     'TestPollingDistrictStreetExpander' : TestPollingDistrictStreetExpander,
     'TestImportLithuanianCounties' : TestImportLithuanianCounties,
     'TestLithuanianConstituencyParser' : TestLithuanianConstituencyParser,
-    'TestAddressParser' : TestAddressParser
+    'TestAddressParser' : TestAddressParser,
+    'TestSeniunaitijaAddressExpander' : TestSeniunaitijaAddressExpander
 }
