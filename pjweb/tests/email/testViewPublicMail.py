3e3ccd784e63abd18afae18c5a1e4cb4bf122b1a
Darius Damalakas <darius.damalakas@gmail.com>
2011-05-27 18:16:53 +0300

    Throwing Http404 if non existent email is found while display public email

---
+++ pjweb/tests/email/testViewPublicMail.py
@@ -0,0 +1,12 @@
+from django.test.client import Client
+from django.test.testcases import TestCase
+
+class TestViewMailList(TestCase):
+    def test_tryToViewNonExistentEmail(self):
+        """ User enters an address, and WTT returns a normal response, no import errors"""
+        c = Client()
+        resp = c.get('/pjweb/public/9999999/')
+
+        # a redirect is generated
+        self.assertEqual(404, resp.status_code)
+        self.assertEqual("", resp.content)
