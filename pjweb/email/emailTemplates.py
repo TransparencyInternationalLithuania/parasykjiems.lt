8eb438f1f8c64a820c39dfd285a0668edb99ecbe
Darius Damalakas <darius.damalakas@gmail.com>
2011-05-27 17:52:34 +0300

    adding English email templates

--- pjweb/email/emailTemplates.py
+++ pjweb/email/emailTemplates.py
@@ -1,8 +1,12 @@
 from django.utils.translation import get_language
 from django.template import loader
 
-def renderEmailTemplate(templateName, templateParams):
-    lang = get_language()
+def renderEmailTemplate(templateName, templateParams, lang = None):
+    # get language from current user setting
+    if lang is None:
+        lang = get_language()
+        if len(lang) > 2:
+            lang = lang[0:2]
     templateName = u"pjweb/emails/%s/%s" % (lang, templateName)
     message = loader.render_to_string(templateName, templateParams)
     return message
--- pjweb/tests/__init__.py
+++ pjweb/tests/__init__.py
@@ -2,6 +2,7 @@ from __future__ import absolute_import
 from pjweb.tests.testFirstPage import TestFirstPage
 from pjweb.tests.testTerritoryPlugin.testSubmitSingleQuery import TestUserEntersAddress, TestDisplayMembersFromStreet
 from pjweb.tests.writeToRepresentative.writeToSingleRepr import TestWriteToSingleRepr, TestWriteToSingleRepr_NoEmail
+from pjweb.tests.email.testRenderMailTemplates import TestRenderAllEmailTemplates_InEveryLanguage
 
 __test__ = {
     'TestFirstPage' : TestFirstPage,
@@ -11,5 +12,8 @@ __test__ = {
 
     # write to representative
     'TestWriteToSingleRepr' : TestWriteToSingleRepr,
-    'TestWriteToSingleRepr_NoEmail' : TestWriteToSingleRepr_NoEmail
+    'TestWriteToSingleRepr_NoEmail' : TestWriteToSingleRepr_NoEmail,
+
+    # email
+    'TestRenderAllEmailTemplates_InEveryLanguage' : TestRenderAllEmailTemplates_InEveryLanguage
 }
