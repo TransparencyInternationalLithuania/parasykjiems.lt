4859df847d3edd1d8e08e7594cc46af3a7413e72
Darius Damalakas <darius.damalakas@gmail.com>
2011-06-03 13:27:37 +0300

    extracting contacts from mayor administartion

--- cdb_lt_streets/management/commands/lrvalstybeCrawl.py
+++ cdb_lt_streets/management/commands/lrvalstybeCrawl.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 from django.core.management.base import BaseCommand
-from cdb_lt_streets.crawlers.LRValstybe_lt.lrvalstybe_crawler import MunicipalityListReader, MunicipalityPageReader, MunicipalityContactReader
+from cdb_lt_streets.crawlers.LRValstybe_lt.lrvalstybe_crawler import MunicipalityListReader, MunicipalityPageReader, MunicipalityContactReader, CivilParishListReader
 from pjutils.timemeasurement import TimeMeasurer
 import logging
 from pjutils.uniconsole import *
@@ -19,12 +19,26 @@ class Command(BaseCommand):
 
         crawler = MunicipalityListReader(u"http://www.lrvalstybe.lt/savivaldybes-4906/")
 
-        for text, url in list(crawler.getMunicipalityList())[46:47]:
+        for text, url in list(crawler.getMunicipalityList()):
             print "%s - %s" % (text, url)
+
 
             municipalityUrl = u"%s%s" % ("http://www.lrvalstybe.lt", url)
             reader = MunicipalityPageReader(municipalityUrl)
-            mayorText, mayorUrl = reader.getMayorUrl()
+            civilParishText, civilParishUrl =  reader.getCivilParishListUrl()
+            print "url %s - %s" % (civilParishText, civilParishUrl)
+
+            if civilParishUrl is None:
+                parish, url = CivilParishListReader(municipalityUrl).getSingleCivilParish()
+                print "CivilParish %s - %s" % (parish, url)
+            else:
+                civilParishUrl = u"%s%s" % ("http://www.lrvalstybe.lt", civilParishUrl)
+                for parish, url in CivilParishListReader(civilParishUrl).getCivilParishList():
+                    print "CivilParish %s - %s" % (parish, url)
+
+
+
+            """mayorText, mayorUrl = reader.getMayorUrl()
             print "Mayor %s - %s" % (mayorText, mayorUrl)
 
 
@@ -32,6 +46,9 @@ class Command(BaseCommand):
             reader = MunicipalityContactReader(mayorUrl)
             for c in reader.getContactList():
                 print "Contact: %s %s %s" % (c.name, c.title, c.email)
+            """
+
             print "\n"
+
 
         print "finished. Took %s seconds" % timeMeasurer.ElapsedSeconds()
