63502605b072245683b036e39ff06e0d95d9c92a
Darius Damalakas <darius.damalakas@intelliflo.com>
2011-06-03 13:50:42 +0300

    extracted civil parish contact list

--- cdb_lt_streets/management/commands/lrvalstybeCrawl.py
+++ cdb_lt_streets/management/commands/lrvalstybeCrawl.py
@@ -19,30 +19,31 @@ class Command(BaseCommand):
 
         crawler = MunicipalityListReader(u"http://www.lrvalstybe.lt/savivaldybes-4906/")
 
-        for text, url in list(crawler.getMunicipalityList()):
-            print "%s - %s" % (text, url)
+        for text, municipalityUrl in list(crawler.getMunicipalityList())[0:1]:
+            print "%s - %s" % (text, municipalityUrl)
 
 
-            municipalityUrl = u"%s%s" % ("http://www.lrvalstybe.lt", url)
             reader = MunicipalityPageReader(municipalityUrl)
             civilParishText, civilParishUrl =  reader.getCivilParishListUrl()
             print "url %s - %s" % (civilParishText, civilParishUrl)
 
-            if civilParishUrl is None:
-                parish, url = CivilParishListReader(municipalityUrl).getSingleCivilParish()
+
+
+            for parish, url in CivilParishListReader(municipalityUrl, civilParishUrl).getCivilParishList():
                 print "CivilParish %s - %s" % (parish, url)
-            else:
-                civilParishUrl = u"%s%s" % ("http://www.lrvalstybe.lt", civilParishUrl)
-                for parish, url in CivilParishListReader(civilParishUrl).getCivilParishList():
-                    print "CivilParish %s - %s" % (parish, url)
+                if url is None:
+                    continue
+                reader = MunicipalityContactReader(url)
+                for c in reader.getContactList():
+                    print "Civpar contact: %s %s %s" % (c.name, c.title, c.email)
 
 
 
-            """mayorText, mayorUrl = reader.getMayorUrl()
+            """
+            mayorText, mayorUrl = reader.getMayorUrl()
             print "Mayor %s - %s" % (mayorText, mayorUrl)
 
 
-            mayorUrl = u"%s%s" % ("http://www.lrvalstybe.lt", mayorUrl)
             reader = MunicipalityContactReader(mayorUrl)
             for c in reader.getContactList():
                 print "Contact: %s %s %s" % (c.name, c.title, c.email)
