bcb2065e8939adf99830209b52ab960512d7c1f0
Darius Damalakas <darius.damalakas@gmail.com>
2011-06-13 11:06:18 +0300

    returning diff data as csv file

---
+++ cdb_lt/dataUpdate/dataUpdate.py
@@ -0,0 +1,167 @@
+import csv
+from contactdb.models import Institution, PersonPosition
+from pjutils.exc import ChainnedException
+from cdb_lt.personUtils import splitIntoNameAndSurname
+from django.http import HttpResponse
+
+class FieldNotDefinedInDataFile(ChainnedException):
+    pass
+
+def toUnicode(val):
+    return unicode(val, 'utf-8')
+
+class InstitutionCache:
+    def __init__(self):
+        self.cache = {}
+
+    def addToCache(self, institutions):
+        for i in institutions:
+            self.cache[i.name] = i
+
+    def loadAllWithName(self, institutionNames, institutionType):
+        step = 30
+        chunks = [institutionNames[i:i+step] for i in range(0, len(institutionNames), step)]
+        for chunk in chunks:
+            institutions = list(Institution.objects.filter(institutionType__code = institutionType) \
+                .filter(name__in=chunk))
+            self.addToCache(institutions)
+
+def getOrDefault(object, property):
+    return getattr(object, property, u"")
+
+
+class PersonPositionCache:
+    def __init__(self):
+        self.cache = {}
+
+    def addToCache(self, personPositions):
+        for p in personPositions:
+            self.cache[p.institution_id] = p
+
+    def loadAllWithInstitutionId(self, institutionIds):
+        step = 30
+        chunks = [institutionIds[i:i+step] for i in range(0, len(institutionIds), step)]
+        for chunk in chunks:
+            personPositions = list(PersonPosition.objects.filter(institution__id__in = chunk).select_related("person"))
+            self.addToCache(personPositions)
+
+
+
+class DataUpdateDiffer:
+    def __init__(self, fileName, institutionType = None, institutionNameGetter = None):
+        self.memberList = self._readCsvFile(fileName=fileName, institutionType=institutionType, institutionNameGetter=institutionNameGetter)
+        self.changedFields = {}
+
+    def _readCsvFile(self, fileName, institutionType = None, institutionNameGetter = None):
+        """ Reads a generic csv file, and builds a list of rows, where
+        each row is a dictionary with data
+
+        institutionType is used to distinguish which
+        institutionNameGetter is used to construct institutionName field, which is an aggregate of several fields usually"""
+        newMayorList = []
+        for row in csv.DictReader(open(fileName, "rt")):
+            val = dict([(toUnicode(key), toUnicode(val)) for key, val in row.iteritems()])
+
+            if val.has_key(u"fullname"):
+                val[u"name"], val[u"surname"] = splitIntoNameAndSurname(val[u"fullname"])
+                del val[u"fullname"]
+
+            if institutionType is not None:
+                val[u"institutionType"] = institutionType
+            else:
+                if not val.has_key(u"institutionType"):
+                    raise FieldNotDefinedInDataFile(u"No institution type defined for row: %s" % row)
+
+            if institutionNameGetter is not None:
+                name = institutionNameGetter(row)
+                val[u"institutionName"] = name
+            else:
+                if not val.has_key(u"institutionName"):
+                    raise FieldNotDefinedInDataFile(u"No institutionName defined for row: %s" % row)
+
+            newMayorList.append(val)
+        return newMayorList
+
+    def getHeaders(self):
+        if not len(self.memberList):
+            return []
+        return self.memberList[0].keys()
+
+    def getHeadersWithChangedFields(self):
+        return self.changedFields.keys()
+
+    def updateIfChanged(self, row, key, newValue, originalValue):
+        if originalValue == newValue:
+            return
+        d = {u"previous": originalValue,
+             u"changed": newValue}
+        row[key] = d
+        self.changedFields[key] = key
+
+    def _buildUpChangedHeaders(self, originalHeaders, changedHeaders):
+        headers = originalHeaders
+        for k in changedHeaders:
+            header = "%s_new" % k
+            headers.append(header)
+        return headers
+
+    def asCsvToResponse(self, attachmentName):
+        """ writes all member data as csv file to a response object and returns it.
+
+        Columns which have changed value will be split into two columns with names: columnName and columnName_new
+
+        Second column will contain new, proposed value
+        """
+        
+        # Create the HttpResponse object with the appropriate CSV header.
+        response = HttpResponse(mimetype='text/csv')
+        response['Content-Disposition'] = 'attachment; filename=%s' % attachmentName
+
+        headers = self._buildUpChangedHeaders(self.getHeaders(), self.getHeadersWithChangedFields())
+        headers.sort()
+        writer = csv.DictWriter(response, fieldnames=headers, extrasaction='ignore')
+        writer.writerow(dict((k, k) for k in headers))
+
+        for row in self.memberList:
+            for key, val in row.items():
+                if type(val) is dict:
+                    keynew = u"%s_new" % key
+                    row[keynew] = val[u'changed'].encode('utf-8')
+                    row[key] = val['previous'].encode('utf-8')
+                else:
+                    row[key] = val.encode('utf-8')
+            writer.writerow(row)
+        return response
+
+    def addChangedFields(self):
+        errorList = []
+        institutionCache = InstitutionCache()
+        institutionNames = [row[u"institutionName"] for row in self.memberList]
+        institutionCache.loadAllWithName(institutionNames, institutionType= self.memberList[0][u"institutionType"])
+
+        personPositionCache = PersonPositionCache()
+        institutionIds = [i.id for i in institutionCache.cache.values()]
+        personPositionCache.loadAllWithInstitutionId(institutionIds)
+
+        for row in self.memberList:
+            institutionName = row[u"institutionName"]
+            if not institutionCache.cache.has_key(institutionName):
+                message = u'Institution with name "%s" not found' % institutionName
+                errorList.append(message)
+                continue
+
+            institutionObj = institutionCache.cache[institutionName]
+
+            previousPersonPosition = None
+            if personPositionCache.cache.has_key(institutionObj.id):
+                previousPersonPosition = personPositionCache.cache[institutionObj.id]
+            previousPerson = getOrDefault(previousPersonPosition, u"person")
+
+
+            self.updateIfChanged(row, u"name", row[u"name"], getOrDefault(previousPerson, u"name"))
+            self.updateIfChanged(row, u"surname", row[u"surname"], getOrDefault(previousPerson, u"surname"))
+            self.updateIfChanged(row, u"institutionName", row[u"institutionName"], institutionObj.name)
+            self.updateIfChanged(row, u"officephone", row[u"officephone"], getOrDefault(previousPersonPosition, u"primaryPhone"))
+            self.updateIfChanged(row, u"officeaddress", row[u"officeaddress"], institutionObj.officeAddress)
+
+        self.errorList = errorList
--- cdb_lt/views.py
+++ cdb_lt/views.py
@@ -1,15 +1,10 @@
 import csv
 import os
 from django.shortcuts import render_to_response
+from cdb_lt.dataUpdate.dataUpdate import DataUpdateDiffer
 from cdb_lt.management.commands.createMembers import makeCivilParishInstitutionName, makeMunicipalityInstitutionName
-from cdb_lt.personUtils import splitIntoNameAndSurname
-from contactdb.models import Institution, Person, PersonPosition
-from pjutils.exc import ChainnedException
 from pjutils.timemeasurement import TimeMeasurer
 from settings import GlobalSettings
-
-class FieldNotDefinedInDataFile(ChainnedException):
-    pass
 
 defaultParams = {
         'LANGUAGES': GlobalSettings.LANGUAGES,
@@ -18,154 +13,56 @@ defaultParams = {
         'step3': '',
     }
 
-def toUnicode(val):
-    return unicode(val, 'utf-8')
-
-def updateIfChanged(row, key, newValue, originalValue):
-    if originalValue == newValue:
-        return
-    d = {u"previous": originalValue,
-         u"changed": newValue}
-    row[key] = d
-
 def joinParams(params):
     viewParams = defaultParams.copy()
     viewParams.update(params)
     return viewParams
 
+civilParishFileName = os.path.join(os.path.realpath(os.path.curdir), "static", "data", "update", "seniunai.csv")
+mayorCsv = os.path.join(os.path.realpath(os.path.curdir), "static", "data", "update", "merai.csv")
+
 def civilParishUpdate(request):
     elapsedTime = TimeMeasurer()
-    fileName = os.path.join(os.path.realpath(os.path.curdir), "static", "data", "update", "seniunai.csv")
-    if not os.path.exists(fileName):
+    if not os.path.exists(civilParishFileName):
         params = {u"ErrorMessage" : "Civil parish file does not exist, please upload it first."}
         return render_to_response('pjweb/error.html', joinParams(params))
 
-    memberList = readCsvFile(fileName, institutionType = u"civpar", institutionNameGetter=makeCivilParishInstitutionName)
-    errorList = addChangedFields(memberList)
-    headers = getHeaders(memberList)
+    differ = DataUpdateDiffer(civilParishFileName, institutionType = u"civpar", institutionNameGetter=makeCivilParishInstitutionName)
+    differ.addChangedFields()
 
-    params = {u"headers" : headers, u"newData" : memberList, u"errorList" : errorList}
+    params = {u"headers" : differ.getHeaders(), u"newData" : differ.memberList, u"errorList" : differ.errorList}
+    response = render_to_response('cdb_lt/update/mayorUpdate.html', joinParams(params))
     print u"generated in %s seconds" % elapsedTime.ElapsedSeconds()
-    return render_to_response('cdb_lt/update/mayorUpdate.html', joinParams(params))
+    return response
 
-def readCsvFile(fileName, institutionType = None, institutionNameGetter = None):
-    """ Reads a generic csv file, and builds a list of rows, where
-    each row is a dictionary with data
+def civilParishUpdateAsCsv(request):
+    if not os.path.exists(civilParishFileName):
+        params = {u"ErrorMessage" : "CivilParish file does not exist, please upload it first."}
+        return render_to_response('pjweb/error.html', joinParams(params))
 
-    institutionType is used to distinguish which
-    institutionNameGetter is used to construct institutionName field, which is an aggregate of several fields usually"""
-    newMayorList = []
-    for row in csv.DictReader(open(fileName, "rt")):
-        val = dict([(toUnicode(key), toUnicode(val)) for key, val in row.iteritems()])
+    differ = DataUpdateDiffer(civilParishFileName, institutionType = u"civpar", institutionNameGetter=makeCivilParishInstitutionName)
+    differ.addChangedFields()
 
-        if val.has_key(u"fullname"):
-            val[u"name"], val[u"surname"] = splitIntoNameAndSurname(val[u"fullname"])
-            del val[u"fullname"]
-
-        if institutionType is not None:
-            val[u"institutionType"] = institutionType
-        else:
-            if not val.has_key(u"institutionType"):
-                raise FieldNotDefinedInDataFile(u"No institution type defined for row: %s" % row)
-
-        if institutionNameGetter is not None:
-            name = institutionNameGetter(row)
-            val[u"institutionName"] = name
-        else:
-            if not val.has_key(u"institutionName"):
-                raise FieldNotDefinedInDataFile(u"No institutionName defined for row: %s" % row)
-
-        newMayorList.append(val)
-    return newMayorList
-
-def getOrDefault(object, property):
-    return getattr(object, property, u"")
+    return differ.asCsvToResponse("civilparish.csv")
 
 
-class InstitutionCache:
-    def __init__(self):
-        self.cache = {}
-
-    def addToCache(self, institutions):
-        for i in institutions:
-            self.cache[i.name] = i
-
-    def loadAllWithName(self, institutionNames, institutionType):
-        step = 30
-        chunks = [institutionNames[i:i+step] for i in range(0, len(institutionNames), step)]
-        for chunk in chunks:
-            institutions = list(Institution.objects.filter(institutionType__code = institutionType) \
-                .filter(name__in=chunk))
-            self.addToCache(institutions)
-
-class PersonPositionCache:
-    def __init__(self):
-        self.cache = {}
-
-    def addToCache(self, personPositions):
-        for p in personPositions:
-            self.cache[p.institution_id] = p
-
-    def loadAllWithInstitutionId(self, institutionIds):
-        step = 30
-        chunks = [institutionIds[i:i+step] for i in range(0, len(institutionIds), step)]
-        for chunk in chunks:
-            personPositions = list(PersonPosition.objects.filter(institution__id__in = chunk).select_related("person"))
-            self.addToCache(personPositions)
-
-
-
-
-def addChangedFields(memberList):
-    errorList = []
-    institutionCache = InstitutionCache()
-    institutionNames = [row[u"institutionName"] for row in memberList]
-    institutionCache.loadAllWithName(institutionNames, institutionType= memberList[0][u"institutionType"])
-
-    personPositionCache = PersonPositionCache()
-    institutionIds = [i.id for i in institutionCache.cache.values()]
-    personPositionCache.loadAllWithInstitutionId(institutionIds)
-
-    for row in memberList:
-        institutionName = row[u"institutionName"]
-        if not institutionCache.cache.has_key(institutionName):
-            message = u'Institution with name "%s" not found' % institutionName
-            errorList.append(message)
-            continue
-
-        institutionObj = institutionCache.cache[institutionName]
-
-        previousPersonPosition = None
-        if personPositionCache.cache.has_key(institutionObj.id):
-            previousPersonPosition = personPositionCache.cache[institutionObj.id]
-        previousPerson = getOrDefault(previousPersonPosition, u"person")
-
-
-        updateIfChanged(row, u"name", row[u"name"], getOrDefault(previousPerson, u"name"))
-        updateIfChanged(row, u"surname", row[u"surname"], getOrDefault(previousPerson, u"surname"))
-        updateIfChanged(row, u"institutionName", row[u"institutionName"], institutionObj.name)
-        updateIfChanged(row, u"officephone", row[u"officephone"], getOrDefault(previousPersonPosition, u"primaryPhone"))
-        updateIfChanged(row, u"officeaddress", row[u"officeaddress"], institutionObj.officeAddress)
-
-    return errorList
-
-def getHeaders(memberList):
-    headers = []
-    if len(memberList) > 0:
-        headers = memberList[0].keys()
-    return headers
-
-
-def mayorUpdate(request):
-
-    fileName = os.path.join(os.path.realpath(os.path.curdir), "static", "data", "update", "merai.csv")
-    if not os.path.exists(fileName):
+def mayorUpdateAsCsv(request):
+    if not os.path.exists(mayorCsv):
         params = {u"ErrorMessage" : "Mayor file does not exist, please upload it first."}
         return render_to_response('pjweb/error.html', joinParams(params))
 
-    newMayorList = readCsvFile(fileName, institutionType = u"mayor", institutionNameGetter=makeMunicipalityInstitutionName)
-    errorList = addChangedFields(newMayorList)
-    headers = getHeaders(newMayorList)
+    differ = DataUpdateDiffer(mayorCsv, institutionType = u"mayor", institutionNameGetter=makeMunicipalityInstitutionName)
+    differ.addChangedFields()
 
-    params = {u"headers" : headers, u"newData" : newMayorList, u"errorList" : errorList}
+    return differ.asCsvToResponse("mayor.csv")
+
+def mayorUpdate(request):
+    if not os.path.exists(mayorCsv):
+        params = {u"ErrorMessage" : "Mayor file does not exist, please upload it first."}
+        return render_to_response('pjweb/error.html', joinParams(params))
+
+    differ = DataUpdateDiffer(mayorCsv, institutionType = u"mayor", institutionNameGetter=makeMunicipalityInstitutionName)
+    differ.addChangedFields()
+
+    params = {u"headers" : differ.getHeaders(), u"newData" : differ.memberList, u"errorList" : differ.errorList}
     return render_to_response('cdb_lt/update/mayorUpdate.html', joinParams(params))
