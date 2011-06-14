2665c39ad3e4baf4b96b27003d88cde6964906ad
Darius Damalakas <darius.damalakas@intelliflo.com>
2011-06-14 17:51:31 +0300

    Uploading csv file containing member data

--- cdb_lt/views.py
+++ cdb_lt/views.py
@@ -1,10 +1,14 @@
+from distutils import dir_util
 import os
 from django.contrib.sites.models import Site
+from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response
 from cdb_lt.dataUpdate.dataUpdate import DataUpdateDiffer
+from cdb_lt.dataUpdate.dataUpload import UploadFileForm
 from cdb_lt.management.commands.createMembers import makeCivilParishInstitutionName, makeMunicipalityInstitutionName
 from pjutils.timemeasurement import TimeMeasurer
 from settings import GlobalSettings
+from settings_local import STATIC_DOC_ROOT
 
 defaultParams = {
         'LANGUAGES': GlobalSettings.LANGUAGES,
@@ -29,6 +33,46 @@ civilParishStatic = os.path.join("data", "update", "seniunai.csv")
 civilParishFileName = os.path.join(os.path.realpath(os.path.curdir), "static", civilParishStatic)
 mayorStatic = os.path.join("data", "update", "merai.csv")
 mayorCsv = os.path.join(os.path.realpath(os.path.curdir), "static", mayorStatic)
+
+def getNonExistentFileName(rootDir, originalFileName):
+    name = os.path.join(rootDir, originalFileName)
+    if os.path.exists(name) == False:
+        return name
+    basename, extension = os.path.splitext(originalFileName)
+    num = 1
+    while True:
+        name = "%s-%s%s" % (basename, num, extension)
+        realName = os.path.join(rootDir, name)
+        if os.path.exists(realName) == False:
+            return name
+        num += 1
+
+def writeUploadedFile(file):
+    """ Writes uploaded file to disk, and returns file name used to store it"""
+    rootDir = os.path.join(STATIC_DOC_ROOT, "dataUpload")
+    dir_util.mkpath(rootDir)
+    fileName = getNonExistentFileName(rootDir=rootDir, originalFileName=file.name)
+    realName = os.path.join(rootDir, fileName)
+    destination = open(realName, 'wb+')
+    for chunk in file.chunks():
+        destination.write(chunk)
+    destination.close()
+    return fileName
+    
+
+def uploadData(request):
+    if request.method != 'POST':
+        form = UploadFileForm()
+        return render_to_response('cdb_lt/update/upload.html', {'form': form})
+
+    form = UploadFileForm(request.POST, request.FILES)
+    if not form.is_valid():
+        form = UploadFileForm()
+        return render_to_response('cdb_lt/update/upload.html', {'form': form})
+
+    returnedFileName = writeUploadedFile(request.FILES['csvFile'])
+    return HttpResponseRedirect('/cdb_lt/update/upload/%s/' % returnedFileName)
+
 
 def civilParishUpdate(request):
     elapsedTime = TimeMeasurer()
