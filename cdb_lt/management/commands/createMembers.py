91cd9ae03af91edc7ef8d6773281347349fc3a54
Darius Damalakas <darius.damalakas@intelliflo.com>
2011-06-28 17:47:37 +0300

    Changing institutionType codes to be more concrete, not just several letter acronyms. findDuplicatePersons will print out institutionType code

--- cdb_lt/management/commands/createMembers.py
+++ cdb_lt/management/commands/createMembers.py
@@ -27,11 +27,14 @@ def cityNameGetterGenitive(csvRow):
     use both interchangeably"""
     return readRow(csvRow, "city_genitive")
 
-InstitutionMunicipalityCode = "mayor"
+InstitutionMunicipalityCode = u"Mayor"
+InstitutionParliamentMember = u"Member of parliament"
+InstitutionCivilparishMembers = u"Civil parish member"
+InstitutionSeniunaitijaMembers = u"Seniūnas"
 
 def loadInstitutionDescriptions():
     institutionDescriptions = {
-            "mp":{
+            InstitutionParliamentMember:{
                 "InstitutionPosition" : u"Seimo narys",
                 "InstitutionResponsibility" : u"Atsakingas už įstatymų leidybą, įstatymu numatytų Lietuvos valstybės institucijų vadovų skyrimą ir atleidimą iš pareigų, valstybės biudžeto tvirtinimą, Lietuvos Respublikos Vyriausybės veiklos priežiūrą."
             },
@@ -39,11 +42,11 @@ def loadInstitutionDescriptions():
                 "InstitutionPosition" : u"Meras",
                 "InstitutionResponsibility" : u"Užtikrina, kad savivaldybės gyventojai turėtų galimybę įsitraukti į vietos reikalų tvarkymą, kad būtų tobulinamas savivaldybės tarybos sprendimų priėmimas ir savivaldybės tarybos komitetų veikla."
             },
-            "civpar":{
+            InstitutionCivilparishMembers:{
                 "InstitutionPosition" : u"Seniūnas",
                 "InstitutionResponsibility" : u"Atlieka seniūnijos vidaus administravimą, prižiūri viešųjų paslaugų teikimą, neatlygintinai atlieka tam tikras notarines funkcijas, konsultuoja seniūnijai priskirtos teritorijos gyventojus, pagal kompetenciją gali surašyti administracinių teisės pažeidimų protokolus, nagrinėti administracinių teisės pažeidimų bylas, gali registruoti mirtis, išduoda leidimus laidoti."
             },
-            "seniunaitija":{
+            InstitutionSeniunaitijaMembers:{
                 "InstitutionPosition" : u"Seniūnaitis",
                 "InstitutionResponsibility" : u"Skatina seniūnaitijos gyventojus prižiūrėti gyvenamosios vietovės teritoriją, plėtoti ir organizuoti vietovės kultūrinį ir sportinį gyvenimą, turi teisę gauti ir teikia informaciją apie savivaldybės institucijų funkcijas, jų darbo laiką ir darbo tvarką, gali atstovauti seniūnaitijos gyventojams savivaldybės tarybos posėdžiuose."
             }
@@ -58,17 +61,17 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
 
         # create MP
-        createInstitutionType(code="mp")
-        importInstitutionData(csvFileName=ImportSourcesMembers.LithuanianMPs, institutionCode = "mp", uniqueKeyStartsFrom=0)
+        createInstitutionType(code=InstitutionParliamentMember)
+        importInstitutionData(csvFileName=ImportSourcesMembers.LithuanianMPs, institutionCode = InstitutionParliamentMember, uniqueKeyStartsFrom=0)
 
         # create Mayor
         createInstitutionType(code=InstitutionMunicipalityCode)
         importInstitutionData(csvFileName=ImportSourcesMembers.LithuanianMunicipalityMembers, institutionCode = InstitutionMunicipalityCode, uniqueKeyStartsFrom=100000)
 
         # create Civil Parish
-        createInstitutionType(code="civpar")
-        importInstitutionData(csvFileName=ImportSourcesMembers.LithuanianCivilParishMembers, institutionCode = "civpar", institutionNameGetter=makeCivilParishInstitutionName, uniqueKeyStartsFrom=200000)
+        createInstitutionType(code=InstitutionCivilparishMembers)
+        importInstitutionData(csvFileName=ImportSourcesMembers.LithuanianCivilParishMembers, institutionCode = InstitutionCivilparishMembers, institutionNameGetter=makeCivilParishInstitutionName, uniqueKeyStartsFrom=200000)
 
         # create Seniunaitija
-        createInstitutionType(code="seniunaitija")
-        importInstitutionData(csvFileName=ImportSourcesMembers.LithuanianSeniunaitijaMembers, institutionCode = "seniunaitija", institutionNameGetter=makeSeniunaitijaInstitutionName, uniqueKeyStartsFrom=300000)
+        createInstitutionType(code=InstitutionSeniunaitijaMembers)
+        importInstitutionData(csvFileName=ImportSourcesMembers.LithuanianSeniunaitijaMembers, institutionCode = InstitutionSeniunaitijaMembers, institutionNameGetter=makeSeniunaitijaInstitutionName, uniqueKeyStartsFrom=300000)
