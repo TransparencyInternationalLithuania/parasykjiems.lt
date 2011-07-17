a64992172616451e2f75735143288a77e1539069
Kiprianas Spiridonovas <34286+kspi@users.noreply.github.com>
2011-07-17 22:56:39 +0300

    Simplify project.

--- urls.py
+++ urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls.defaults import patterns, include, url
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 admin.autodiscover()
 
-urlpatterns = patterns('',
-    url(r'^$', include('parasykjiems.web.urls')),
+urlpatterns = patterns('views',
+    url(r'^$', 'index'),
+
     url(r'^admin/', include(admin.site.urls)),
 )
