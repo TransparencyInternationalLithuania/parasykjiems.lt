de8eb409a2dd6d627a253f6a14051928d47778e1
Kiprianas Spiridonovas <34286+kspi@users.noreply.github.com>
2011-07-18 10:33:16 +0300

    Create some models.

--- urls.py
+++ urls.py
@@ -1,11 +1,10 @@
 from django.conf.urls.defaults import patterns, include, url
 from django.contrib import admin
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 admin.autodiscover()
 
-urlpatterns = patterns('views',
-    url(r'^$', 'index'),
+urlpatterns = patterns('',
+    url(r'^$', include('web.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
 )
