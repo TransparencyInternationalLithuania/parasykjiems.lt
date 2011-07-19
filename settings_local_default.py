8452b202fda3b69d5818870c4f3745418854708d
Kiprianas Spiridonovas <k.spiridonovas@gmail.com>
2011-07-19 11:26:38 +0300

    Improve importdata, remove data from repository (format may still change), implement local setting system.

---
+++ settings_local_default.py
@@ -0,0 +1,17 @@
+DEBUG = True
+
+ADMINS = (
+    # ('Your Name', 'your_email@example.com'),
+)
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'parasykjiems.db',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+    }
+}
