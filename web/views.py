8ab7908880657e0068be661bc4ac379d2e9b8967
Kiprianas Spiridonovas <k.spiridonovas@gmail.com>
2011-07-21 13:35:58 +0300

    Improve and simplify menu rendering.

--- web/views.py
+++ web/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
+from django.utils.translation import ugettext as _
 
 def index(request):
     return render(request, 'index.html', {
         'search_query': request.GET.get('q', ''),
+        'active_menu': _('Find representative'),
     })
 
 def mail(request):
-    return render(request, 'mail.html')
+    return render(request, 'mail.html', {
+        'active_menu': _('Public letters'),
+    })
 
 def about(request):
-    return render(request, 'about.html')
+    return render(request, 'about.html', {
+        'active_menu': _('About project'),
+    })
