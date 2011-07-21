ef1776b7a7cf619e4c2dc4d4f012dcbedbbb3a12
Kiprianas Spiridonovas <34286+kspi@users.noreply.github.com>
2011-07-21 13:47:05 +0300

    Add URL to change language.

--- web/urls.py
+++ web/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls.defaults import patterns, include, url
 
 urlpatterns = patterns('web.views',
     url(r'^$', 'index'),
-    url(r'^mail', 'mail'),
-    url(r'^about', 'about'),
+    url(r'^mail$', 'mail'),
+    url(r'^about$', 'about'),
+
+    url(r'^setlang$', 'setlang'),
 )
