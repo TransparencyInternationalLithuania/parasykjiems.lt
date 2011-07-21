28222c0a736465ddc223ac02d86c4f334bf62d38
Kiprianas Spiridonovas <k.spiridonovas@gmail.com>
2011-07-21 12:45:55 +0300

    Abstract menu into web.menu.

--- urls.py
+++ urls.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^$', include('web.urls')),
+    url(r'', include('web.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
 )
