e0b8adcc036fd55a453c7e69f529cf415918b4f5
Kiprianas Spiridonovas <k.spiridonovas@gmail.com>
2011-07-21 13:47:05 +0300

    Add URL to change language.

--- web/views.py
+++ web/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.utils.translation import ugettext as _
+import django.http
 
 def index(request):
     return render(request, 'index.html', {
@@ -16,3 +17,10 @@ def about(request):
     return render(request, 'about.html', {
         'active_menu': _('About project'),
     })
+
+def setlang(request):
+    language = request.GET.get('lang', 'lt')
+    back = request.META.get('HTTP_REFERER', '/')
+    response = django.http.HttpResponseRedirect(back)
+    request.session['django_language'] = language
+    return response
