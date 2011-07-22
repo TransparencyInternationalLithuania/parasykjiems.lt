6b6fc1395c4920c256ecfcc50f9ef323724e53d1
Kiprianas Spiridonovas <k.spiridonovas@gmail.com>
2011-07-22 14:31:11 +0300

    Add institution and representative views.

--- web/views.py
+++ web/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.utils.translation import ugettext as _
+from django.core.exceptions import ObjectDoesNotExist
 import django.http
+
+from web.models import Representative, Institution
 
 def index(request):
     return render(request, 'index.html', {
@@ -17,6 +20,28 @@ def about(request):
     return render(request, 'about.html', {
         'active_menu': _('About project'),
     })
+
+def representative(request, id):
+    try:
+        rep = Representative.objects.get(id=id)
+        return render(request, 'representative.html', {
+            'representative': rep,
+            'active_menu': _('Representative search'),
+        })
+    except ObjectDoesNotExist:
+        return django.http.HttpResponseNotFound(_('Representative not found'))
+
+def institution(request, id):
+    try:
+        inst = Institution.objects.get(id=id)
+        reps = Representative.objects.filter(institution=inst)
+        return render(request, 'institution.html', {
+            'institution': inst,
+            'representatives': reps,
+            'active_menu': _('Representative search'),
+        })
+    except ObjectDoesNotExist:
+        return django.http.HttpResponseNotFound(_('Institution not found'))
 
 def setlang(request):
     language = request.GET.get('lang', 'lt')
