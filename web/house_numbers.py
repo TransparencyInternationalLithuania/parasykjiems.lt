bb28948114d1c017413ac03661dddbe86caab7c0
Kiprianas Spiridonovas <k.spiridonovas@gmail.com>
2011-07-26 12:29:47 +0300

    Document and add tests to web.house_numbers.

--- web/house_numbers.py
+++ web/house_numbers.py
@@ -14,52 +14,114 @@ def _is_odd(x):
 
 
 _RANGE_CHECKS = {
-    'all': _const_True,
-    'even': _is_even,
-    'odd': _is_odd,
+    '': _const_True,
+    ' even': _is_even,
+    ' odd': _is_odd,
 }
 
 
-_INTERVAL_RE = re.compile(r'(\d+)-(\d+) (all|even|odd)')
-_LEFT_INTERVAL_RE = re.compile(r'(\d+)- (all|even|odd)')
-_SINGLE_RE = re.compile(r'(\d+\w?)')
+_INTERVAL_RE = re.compile(r'^(\d+)-(\d+)(| even| odd)$')
+_LEFT_INTERVAL_RE = re.compile(r'^(\d+)-(| even| odd)$')
+_SINGLE_RE = re.compile(r'^(\d+\w?)$')
 
 
 class HouseNumberSet:
-    """An object which parses a house number range definition string,
-    like '3-15 odd, 4-10 even, 10A', and allows for easy checking if a
-    house number (represented as a string, because it can contains a
-    letter) belongs to any of those intervals.
+    """Given a house number range definition string, allows for easy
+    checking for house number membership in that range.
+
+    A string can contain zero or more items like:
+
+    (1) 'X-Y' - a range of numbers from X to Y, inclusive. X and Y are
+    numbers.
+
+    (2) 'X-Y odd' and 'X-Y even' - like (1), but only includes odd or
+    even numbers respectively.
+
+    (3) 'X-', 'X- odd', 'X- even' - like (1) and (2), but doesn't have
+    an upper limit.
+
+    (4) 'X' - denotes a single house number. May also contain a
+    letter.
+
+    A string with no items means that it contains all house numbers.
+
+    >>> s = HouseNumberSet('3-13 odd, 6-16 even, 10a, 18A, 30-40, 50- odd')
+    >>> 1 in s
+    False
+    >>> 3 in s
+    True
+    >>> '3a' in s
+    False
+    >>> 4 in s
+    False
+    >>> 5 in s
+    True
+    >>> 6 in s
+    True
+
+    >>> '10A' in s
+    True
+    >>> '10a' in s
+    True
+    >>> 18 in s
+    False
+    >>> '18a' in s
+    True
+
+    >>> 30 in s
+    True
+    >>> 31 in s
+    True
+
+    >>> 60 in s
+    False
+    >>> 61 in s
+    True
+
+    >>> s = HouseNumberSet('')
+    >>> '123a' in s
+    True
     """
+
     def __init__(self, string):
-        self.numbers = set()
         self.string = string
 
-        # Intervals are stored as predicate functions.
-        self.intervals = []
+        if string == '':
+            # An empty string is a wildcard.
+            self.universal = True
+        else:
+            self.universal = False
+            self.numbers = set()
+            # Intervals are stored as predicate functions.
+            self.intervals = []
+            for item in string.split(','):
+                item = item.strip()
 
-        for item in string.split(','):
-            item = item.strip()
+                m = _INTERVAL_RE.match(item)
+                if m:
+                    self.add_interval(m.group(1), m.group(2), m.group(3))
+                    continue
 
-            m = _INTERVAL_RE.match(item)
-            if m:
-                self.add_interval(m.group(1), m.group(2), m.group(3))
-                continue
+                m = _LEFT_INTERVAL_RE.match(item)
+                if m:
+                    self.add_left_interval(m.group(1), m.group(2))
+                    continue
 
-            m = _LEFT_INTERVAL_RE.match(item)
-            if m:
-                self.add_left_interval(m.group(1), m.group(2))
-                continue
+                m = _SINGLE_RE.match(item)
+                if m:
+                    self.add_single(m.group(1))
+                    continue
 
-            m = _SINGLE_RE.match(item)
-            if m:
-                self.add_single(m.group(1))
-                continue
+                raise ValueError(
+                    'Bad house number definition string item "{}" in "{}".'
+                    .format(item, string))
 
     def __unicode__(self):
         return self.string
 
     def __contains__(self, x):
+        if self.universal:
+            return True
         xs = str(x)
         if xs.upper() in self.numbers:
             return True
@@ -93,7 +155,7 @@ class HouseNumberSet:
         self.intervals.append(left_interval)
 
     def add_single(self, number):
-        self.numbers.add(number)
+        self.numbers.add(number.upper())
 
 
 ## A map of Territory ids to corresponding HouseNumberSets
@@ -114,4 +176,15 @@ def get_house_number_set(territory):
 
 
 def territory_contains(territory, house_number):
+    """Checks if a territory contains a house number.
+
+    >>> from web.models import Location, Territory
+    >>> t = Territory(location=Location('a', 'b'), numbers='10-14 even')
+    >>> territory_contains(t, 1)
+    False
+    >>> territory_contains(t, 11)
+    False
+    >>> territory_contains(t, 12)
+    True
+    """
     return house_number in get_house_number_set(territory)
