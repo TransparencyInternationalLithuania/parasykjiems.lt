b3be3fa6486eb51ce6a7f75cf6b66b23ff2956bc
Kiprianas Spiridonovas <k.spiridonovas@gmail.com>
2011-07-28 12:23:15 +0300

    Make Enquiry model generate unique random ids.

--- mail/models.py
+++ mail/models.py
@@ -1,3 +1,5 @@
+import random
+
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
@@ -8,7 +10,7 @@ _NAME_LEN = 200
 
 class Enquiry(models.Model):
     parent = models.ForeignKey('Response', null=True)
-    unique_hash = models.CharField(max_length=20)
+    unique_hash = models.IntegerField(primary_key=True, unique=True)
 
     # These link to the institution or representative that this
     # enquiry was sent to. Exactly one of them should be non-null.
@@ -27,6 +29,20 @@ class Enquiry(models.Model):
 
     sent = models.BooleanField()
     sent_time = models.DateTimeField(auto_now_add=True)
+
+    _hash_tries = 10
+    _hash_max = 999999
+
+    def __init__(self, *args, **kwargs):
+        super(Enquiry, self).__init__(*args, **kwargs)
+        if not self.unique_hash:
+            self.unique_hash = random.randint(1, Enquiry._hash_max)
+            tries = 0
+            while Enquiry.objects.filter(unique_hash=self.unique_hash).exists():
+                self.unique_hash = random.randint(1, Enquiry._hash_max)
+                tries += 1
+                if tries > Enquiry._hash_tries:
+                    raise Exception("Probably out of unique hashes for Enquiry.")
 
 
 class Response(models.Model):
