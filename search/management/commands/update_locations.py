53389013444ae935196c103c30b13f44f6543bf1
Kiprianas Spiridonovas <k.spiridonovas@gmail.com>
2011-08-01 15:48:45 +0300

    Add 'update_locations' command.

---
+++ search/management/commands/update_locations.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+from django.core.management.base import BaseCommand
+from django.core.exceptions import ObjectDoesNotExist
+from progressbar import ProgressBar, Bar, ETA
+
+from django.db import connection, transaction
+
+
+class Command(BaseCommand):
+    args = '<>'
+    help = "Updates locations to match territories."
+
+    def handle(self, *args, **options):
+        cursor = connection.cursor()
+
+        # Data modifying operation - commit required
+        cursor.execute('''
+        DELETE FROM search_location;
+        INSERT INTO search_location (municipality, elderate, city, street)
+        SELECT DISTINCT municipality, elderate, city, street
+        FROM search_territory;
+        ''')
+        transaction.commit_unless_managed()
