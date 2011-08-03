c813cc9ff5e7b5a841347725c1f70536efc05b3d
Kiprianas Spiridonovas <34286+kspi@users.noreply.github.com>
2011-08-03 09:56:50 +0300

    Add help view, add breadcrumb to letters view.

--- search/admin.py
+++ search/admin.py
@@ -5,5 +5,28 @@ admin.site.register(models.InstitutionKind)
 admin.site.register(models.Institution)
 admin.site.register(models.RepresentativeKind)
 admin.site.register(models.Representative)
-admin.site.register(models.Territory)
-admin.site.register(models.Location)
+
+
+class TerritoryAdmin(admin.ModelAdmin):
+    list_display = ('institution',
+                    'municipality',
+                    'elderate',
+                    'city',
+                    'street',
+                    'numbers',
+                    )
+    list_display_links = ('institution',)
+    list_editable = ('municipality',
+                    'elderate',
+                    'city',
+                    'street',
+                     )
+
+    search_fields = ('municipality',
+                     'elderate',
+                     'city',
+                     'street',
+                     )
+
+
+admin.site.register(models.Territory, TerritoryAdmin)
