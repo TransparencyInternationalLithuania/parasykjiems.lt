f7f6712dc588b0e2d7f2740b8fe14ef5bbfb9e8b
Kiprianas Spiridonovas <34286+kspi@users.noreply.github.com>
2011-08-05 10:49:50 +0300

    Rename fill_elderates command to tidy_elderates, because now it also removes elderate information from territories with streets.

--- search/management/commands/tidy_elderates.py
+++ search/management/commands/tidy_elderates.py
@@ -1,22 +1,31 @@
 from django.core.management.base import BaseCommand
+from progressbar import ProgressBar, Bar, ETA
 from search import models
 
 
 class Command(BaseCommand):
     args = '<>'
-    help = """Tries to fill empty elderate fields in territories.
+    help = """Tries to tidy elderates in territories.
 
-    Sometimes a street is entered both an elderate and without one for
-    different institutions. This command finds all instances of
+    Sometimes a territory is entered both an elderate and without one
+    for different institutions. This command finds all instances of
     territories with missing elderates and fills them from other
     territories with exact same municipality, city and street.
+
+    This is only done for territories without streets. For territories
+    with streets, the elderate is removed altogether, because the user
+    needn't know his elderate and hopefully he'll use our search to
+    find his elderate by house number.
     """
 
     def handle(self, *args, **options):
-        no_elderate = models.Territory.objects.filter(elderate='')
+        print 'Filling elderates.'
         count = 0
         filled = 0
-        for t in no_elderate:
+        no_elderate = models.Territory.objects.filter(
+            elderate='',
+            street='')
+        for t in ProgressBar(widgets=[ETA(), ' ', Bar()])(no_elderate):
             count += 1
             same_location = (models.Territory.objects
                              .filter(
@@ -34,3 +43,12 @@ class Command(BaseCommand):
                 t.save()
         print 'Filled {} elderates of {} territories missing an elderate.' \
               .format(filled, count)
+        print
+
+        print 'Removing elderates from territories with streets.'
+        streets = models.Territory.objects.exclude(
+            elderate='',
+            street='')
+        for street in ProgressBar(widgets=[ETA(), ' ', Bar()])(streets):
+            street.elderate = ''
+            street.save()
