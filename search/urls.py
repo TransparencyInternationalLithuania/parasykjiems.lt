64bad99ed78273410736da0482fbb910ac35a804
Kiprianas Spiridonovas <k.spiridonovas@gmail.com>
2011-08-08 14:22:04 +0300

    Make slugs the only addressing mechanism for 'search'.

--- search/urls.py
+++ search/urls.py
@@ -3,12 +3,12 @@ from django.conf.urls.defaults import patterns, url
 urlpatterns = patterns('search.views',
     url(r'^$', 'search', name='search'),
 
-    url(r'^institution/(?P<id>[\w-]+)$', 'institution',
+    url(r'^institution/(?P<slug>[\w-]+)$', 'institution',
         name='institution'),
-    url(r'^representative/(?P<id>[\w-]+)$', 'representative',
+    url(r'^representative/(?P<slug>[\w-]+)$', 'representative',
         name='representative'),
 
-    url(r'^location/(?P<id>[\w-]+)$', 'location', name='location'),
-    url(r'^location/(?P<id>[\w-]+)/ask$', 'location_ask'),
-    url(r'^location/(?P<id>[\w-]+)/(?P<house_number>\d+\w?)$', 'location'),
+    url(r'^location/(?P<slug>[\w-]+)$', 'location', name='location'),
+    url(r'^location/(?P<slug>[\w-]+)/ask$', 'location_ask'),
+    url(r'^location/(?P<slug>[\w-]+)/(?P<house_number>\d+\w?)$', 'location'),
 )
