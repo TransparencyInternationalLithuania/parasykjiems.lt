e998ec79fc7f6d5a0de543ed4ba1a0861c81fd95
Kiprianas Spiridonovas <34286+kspi@users.noreply.github.com>
2011-08-08 13:14:32 +0300

    Factor out slug functions into separate module and use slugs for institutions and representatives.

--- search/urls.py
+++ search/urls.py
@@ -3,9 +3,9 @@ from django.conf.urls.defaults import patterns, url
 urlpatterns = patterns('search.views',
     url(r'^$', 'search', name='search'),
 
-    url(r'^institution/(?P<inst_id>\d+)$', 'institution',
+    url(r'^institution/(?P<id>[\w-]+)$', 'institution',
         name='institution'),
-    url(r'^representative/(?P<rep_id>\d+)$', 'representative',
+    url(r'^representative/(?P<id>[\w-]+)$', 'representative',
         name='representative'),
 
     url(r'^location/(?P<id>[\w-]+)$', 'location', name='location'),
