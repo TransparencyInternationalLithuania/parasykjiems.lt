17672bd3d8da5cb6d9252c418779075543f7a4c3
Kiprianas Spiridonovas <34286+kspi@users.noreply.github.com>
2011-08-09 12:07:46 +0300

    Update locations incrementally.

--- search/management/commands/update_locations.py
+++ search/management/commands/update_locations.py
@@ -10,28 +10,48 @@ class Command(BaseCommand):
     help = "Updates locations to match territories."
 
     def handle(self, *args, **options):
-        print "Building locations."
-
         # A complicated SQL query is used for speed. I don't think
         # this is possible to express in Django's ORM.
         #
         # The complicated joins are meant to filter out territories of
         # inactive institutions.
 
-        cursor = connection.cursor()
-        cursor.execute('''
-        DELETE FROM search_location;
+        print 'Before: {} locations.'.format(Location.objects.count())
 
-        INSERT INTO search_location (municipality, elderate, city, street)
-          SELECT DISTINCT municipality, elderate, city, street
+        cursor = connection.cursor()
+
+        print 'Inserting new ones.'
+        cursor.execute('''
+        INSERT INTO search_location (municipality, elderate, city, street, slug)
+          SELECT DISTINCT municipality, elderate, city, street, ''
           FROM
             search_territory
             INNER JOIN search_institution
               ON (search_territory.institution_id = search_institution.id)
             INNER JOIN search_institutionkind
               ON (search_institution.kind_id = search_institutionkind.id)
-          WHERE search_institutionkind.active = true;
+          WHERE search_institutionkind.active = true
+          AND NOT EXISTS
+            (SELECT *
+             FROM search_location
+             WHERE search_location.municipality = search_territory.municipality
+               AND search_location.elderate = search_territory.elderate
+               AND search_location.city = search_territory.city
+               AND search_location.street = search_territory.street);
         ''')
+
+        print 'Deleting old ones.'
+        cursor.execute('''
+        DELETE FROM search_location
+        WHERE NOT EXISTS
+          (SELECT *
+           FROM search_territory
+           WHERE search_location.municipality = search_territory.municipality
+             AND search_location.elderate = search_territory.elderate
+             AND search_location.city = search_territory.city
+             AND search_location.street = search_territory.street);
+        ''')
+
         transaction.commit_unless_managed()
 
-        print 'Created {} locations.'.format(Location.objects.count())
+        print 'After: {} locations.'.format(Location.objects.count())
