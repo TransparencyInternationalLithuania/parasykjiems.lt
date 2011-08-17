063375f53fb41c9faf3a6c6746d2252b897a2197
Kiprianas Spiridonovas <k.spiridonovas@gmail.com>
2011-08-17 15:27:53 +0300

    Implement incremental search instead of autocomplete.

--- static/js/search.js
+++ static/js/search.js
@@ -1,8 +1,40 @@
+function encodeQuery(q) {
+    return encodeURI(q.replace(/ /g, '+'));
+}
+
+function fetchResults(terms) {
+    var results = $('#results');
+    $.ajax({
+               dataType: 'text',
+               url: 'results/',
+               data: { q: terms },
+               success: function(newResults) {
+                   results.html(newResults);
+                   var url;
+                   if (terms == '') {
+                       url = '/';
+                   } else {
+                       url = '/?q=' + encodeQuery(terms);
+                   }
+                   history.replaceState(null, '', url);
+               }
+           });
+}
+
+var resultsTimeout = null;
+
 $(function() {
-      // Focus search box.
-      var q = document.search.q;
+      var q = $(document.search.q);
       q.focus();
-      
-      // Setup autocomplete.
-      $(q).autocomplete({source: "autocomplete"})
-});
+      q.css({width: '100%'});
+
+      q.keyup(function() {
+                  if (resultsTimeout) {
+                      clearTimeout(resultsTimeout);
+                  }
+                  resultsTimeout = setTimeout(function() {
+                                                  fetchResults(q.val());
+                                                  clearTimeout(resultsTimeout);
+                                              }, 600);
+              });
+  });
