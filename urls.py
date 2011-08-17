bfe05e85a963d918044d9839a6958d3ff681a4d8
Kiprianas Spiridonovas <34286+kspi@users.noreply.github.com>
2011-08-17 17:30:10 +0300

    Add content to help view.

--- urls.py
+++ urls.py
@@ -6,9 +6,12 @@ admin.autodiscover()
 
 
 urlpatterns = patterns('',
-    url(r'', include('web.urls')),
     url(r'', include('search.urls')),
     url(r'', include('mail.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+
+    # This should be the last in the list, because the
+    # web.views.article's URL is sort of catch-all.
+    url(r'', include('web.urls')),
 )
