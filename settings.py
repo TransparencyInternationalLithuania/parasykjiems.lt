1f3ad699c064d4a0c91d6a33d3764e84d0e6f59b
Kiprianas Spiridonovas <34286+kspi@users.noreply.github.com>
2011-08-19 13:04:45 +0300

    Display institution and representative descriptions.

--- settings.py
+++ settings.py
@@ -93,10 +93,10 @@ INSTALLED_APPS = (
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
-    'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.admin',
+    'django.contrib.markup',
 
     'haystack',
     'south',
