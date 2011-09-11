966e1a55981ee06eea45e002a12b7d30734bbd5a
Kiprianas Spiridonovas <k.spiridonovas@gmail.com>
2011-09-11 11:09:06 +0300

    Add importer/exporter for kinds.

---
+++ search/management/commands/import_kinds.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+from django.core.management.base import BaseCommand
+from csv_serialization import import_models
+from search import models
+
+
+class Command(BaseCommand):
+    args = '<>'
+    help = '''Imports kind data to "data/kinds/representative.csv" and
+    "data/kinds/institution.csv"'''
+
+    def handle(self, *args, **options):
+        import_models('data/kinds/representative.csv',
+                      models.RepresentativeKind,
+                      ['name'],
+                      ['ordinal', 'description'],
+                      {'active': True})
+        import_models('data/kinds/institution.csv',
+                      models.InstitutionKind,
+                      ['name'],
+                      ['ordinal', 'description'],
+                      {'active': True})
