4cfe12ab988838867fc095c8eb9324b80bb45088
Kiprianas Spiridonovas <34286+kspi@users.noreply.github.com>
2011-09-12 19:14:02 +0300

    Make 'process_message' command obey settings.LANGUAGE_CODE.

--- mail/management/commands/process_message.py
+++ mail/management/commands/process_message.py
@@ -1,7 +1,10 @@
 import email
 import sys
 from django.core.management.base import BaseCommand
+from django.utils import translation
+
 from parasykjiems.mail import mail
+import settings
 
 
 class Command(BaseCommand):
@@ -11,5 +14,9 @@ class Command(BaseCommand):
     '''
 
     def handle(self, *args, **options):
+        # For some reason this is needed to set the language for
+        # translation strings inside the command.
+        translation.activate(settings.LANGUAGE_CODE)
+
         msg = email.message_from_file(sys.stdin)
         mail.process_incoming(msg)
