55c5dbcac855f257381be9543e94cd997088ed44
Kiprianas Spiridonovas <34286+kspi@users.noreply.github.com>
2011-09-12 18:49:55 +0300

    Don't use LocaleMiddleware.

--- middleware.py
+++ middleware.py
@@ -1,19 +1,3 @@
-class ForceDefaultLanguageMiddleware(object):
-    """Ignore Accept-Language HTTP headers
-
-    This will force the I18N machinery to always choose
-    settings.LANGUAGE_CODE as the default initial language, unless
-    another one is set via sessions or cookies
-
-    Should be installed *before* any middleware that checks
-    request.META['HTTP_ACCEPT_LANGUAGE'], namely
-    django.middleware.locale.LocaleMiddleware
-    """
-    def process_request(self, request):
-        if 'HTTP_ACCEPT_LANGUAGE' in request.META:
-            del request.META['HTTP_ACCEPT_LANGUAGE']
-
-
 class SetRemoteAddrMiddleware(object):
     """If X-Real-IP header is set, set REMOTE_ADDR to its contents.
 
