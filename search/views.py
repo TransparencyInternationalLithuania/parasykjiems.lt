c78e5bf8684cab53d90815643a15c7eb7b2a8570
Kiprianas Spiridonovas <34286+kspi@users.noreply.github.com>
2011-09-12 20:10:23 +0300

    More logging improvements.

--- search/views.py
+++ search/views.py
@@ -23,7 +23,7 @@ def search(request):
     if 'q' in request.GET and request.GET['q'] != '':
         q = request.GET['q']
 
-        logger.info('SEARCH "{}"'.format(q))
+        logger.info('SEARCH {}'.format(q))
 
         q, num = utils.remove_house_number(q)
 
