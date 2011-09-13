e2b159af38660e933f07d4f63f00eec8af17e849
Kiprianas Spiridonovas <k.spiridonovas@gmail.com>
2011-09-13 20:44:15 +0300

    Improve 'web' admin interface.

--- web/admin.py
+++ web/admin.py
@@ -1,5 +1,29 @@
 from django.contrib import admin
 import models
 
-admin.site.register(models.Article)
-admin.site.register(models.Message)
+
+def summary(s):
+    short = s[:80]
+    if short != s:
+        short += '...'
+    return short.replace('\n', '')
+
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('location', 'title', 'body_summary')
+    list_display_links = list_display
+
+    def body_summary(self, obj):
+        return summary(obj.body)
+
+admin.site.register(models.Article, ArticleAdmin)
+
+
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('name', 'body_summary')
+    list_display_links = list_display
+
+    def body_summary(self, obj):
+        return summary(obj.body)
+
+admin.site.register(models.Message, MessageAdmin)
