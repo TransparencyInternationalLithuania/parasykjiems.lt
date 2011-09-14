c474df425d7a34dd5cedf62d842ef95fd4b0d4bc
Kiprianas Spiridonovas <34286+kspi@users.noreply.github.com>
2011-09-14 22:09:22 +0300

    Improve remote addr middleware.

--- middleware.py
+++ middleware.py
@@ -1,9 +1,10 @@
 class SetRemoteAddrMiddleware(object):
-    """If X-Real-IP header is set, set REMOTE_ADDR to its contents.
+    """Set REMOTE_ADDR behind proxy.
 
-    This should be used if the application is launched behind a remove
-    proxy that sets X-Real-IP.
+    Uses X-Forwarded-For and X-Real-IP, in that order.
     """
     def process_request(self, request):
+        if 'HTTP_X_FORWARDED_FOR' in request.META:
+            request.META['REMOTE_ADDR'] = request.META['HTTP_X_FORWARDED_FOR']
         if 'HTTP_X_REAL_IP' in request.META:
             request.META['REMOTE_ADDR'] = request.META['HTTP_X_REAL_IP']
