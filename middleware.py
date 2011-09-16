3f3048608d9fc1983be49cff77055eb2f7f0983e
Kiprianas Spiridonovas <34286+kspi@users.noreply.github.com>
2011-09-16 21:54:37 +0300

    Improve feedback handling.

--- middleware.py
+++ middleware.py
@@ -6,5 +6,5 @@ class SetRemoteAddrMiddleware(object):
     def process_request(self, request):
         if 'HTTP_X_FORWARDED_FOR' in request.META:
             request.META['REMOTE_ADDR'] = request.META['HTTP_X_FORWARDED_FOR']
-        if 'HTTP_X_REAL_IP' in request.META:
+        elif 'HTTP_X_REAL_IP' in request.META:
             request.META['REMOTE_ADDR'] = request.META['HTTP_X_REAL_IP']
