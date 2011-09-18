995e7e2fcf2b9e21bc9966582823b248b082d256
Kiprianas Spiridonovas <34286+kspi@users.noreply.github.com>
2011-09-18 18:06:51 +0300

    Enable sites framework.

--- settings.py
+++ settings.py
@@ -99,6 +99,7 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     'django.contrib.markup',
     'django.contrib.humanize',
+    'django.contrib.sites',
     'django.contrib.sitemaps',
 
     'haystack',
