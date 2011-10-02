2e8e055374f9605395f20893b9bccf6269ccea8c
Kiprianas Spiridonovas <k.spiridonovas@gmail.com>
2011-10-02 13:31:24 +0300

    Delete territories before importing, so that there are no duplicates.

--- search/management/commands/import_territories.py
+++ search/management/commands/import_territories.py
@@ -17,7 +17,12 @@ class Command(BaseCommand):
             print '({:2}/{:2})'.format(i + 1, len(MUNICIPALITIES)),
             filename = os.path.join('data', 'territories',
                                     municipality.encode('utf-8') + '.csv')
+
+            # Delete all territories for this municipality before
+            # importing, so that they don't double. We can't really
+            # update them in any other way.
             models.Territory.objects.filter(municipality=municipality).delete()
+
             import_models(filename,
                           models.Territory,
                           [('institution', 'institution__name'),
