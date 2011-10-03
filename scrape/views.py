6f2caaeb23017e72e1234982227f4c6a437d48ef
Kiprianas Spiridonovas <k.spiridonovas@gmail.com>
2011-10-03 20:30:03 +0300

    Improve data update view.

--- scrape/views.py
+++ scrape/views.py
@@ -5,7 +5,8 @@ from django.contrib.auth.decorators import login_required
 
 @login_required(login_url='/admin/', redirect_field_name=None)
 def admin_update(request):
-    changes = models.RepresentativeChange.objects.all()
+    changes = models.RepresentativeChange.objects.all().order_by(
+        'institution', 'kind_name')
     return render(request, 'views/admin_update.html', {
         'changes': changes,
     })
