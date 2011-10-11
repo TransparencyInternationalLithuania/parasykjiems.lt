cb376c12f1f4f9818f53997edd5abdf142b31509
Kiprianas Spiridonovas <34286+kspi@users.noreply.github.com>
2011-10-11 22:43:47 +0300

    Fix mail sitemaps, admin, feeds.

--- mail/admin.py
+++ mail/admin.py
@@ -2,5 +2,6 @@ from django.contrib import admin
 from parasykjiems.mail import models
 
 
-admin.site.register(models.Enquiry)
-admin.site.register(models.Response)
+admin.site.register(models.UnconfirmedMessage)
+admin.site.register(models.Message)
+admin.site.register(models.Thread)
