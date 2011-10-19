3c476e68e61b5de29c3e9523554bcedda92a9c38
Kiprianas Spiridonovas <k.spiridonovas@gmail.com>
2011-10-19 18:11:41 +0300

    Fix 'sent' view for non-public threads.

--- mail/urls.py
+++ mail/urls.py
@@ -11,7 +11,7 @@ urlpatterns = patterns('mail.views',
 
     url(r'^confirm/(?P<id>\d+)/(?P<confirm_secret>\d+)/$', 'confirm',
         name='confirm'),
-    url(r'^sent/(?P<slug>[\w-]+)/$', 'sent'),
+    url(r'^sent/(?P<id>[\w-]+)/$', 'sent'),
 
     url(r'^thread/(?P<slug>[\w-]+)/$', 'thread', name='thread'),
 
