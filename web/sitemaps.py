861b303f777a21d829db429b5da3aead69653354
Kiprianas Spiridonovas <k.spiridonovas@gmail.com>
2011-10-23 11:04:26 +0300

    Deprioritize privacy policy and disclaimer articles in sitemap.

--- web/sitemaps.py
+++ web/sitemaps.py
@@ -4,7 +4,12 @@ from web.models import Article
 
 class ArticleSitemap(Sitemap):
     changefreq = "monthly"
-    priority = 0.5
 
     def items(self):
         return Article.objects.all()
+
+    def priority(self, obj):
+        if obj.location in ['privacy', 'disclaimer']:
+            return 0.1
+        else:
+            return 0.6
