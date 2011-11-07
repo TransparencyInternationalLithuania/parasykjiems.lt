9f02ee78cd754618194cca433dc0d14c48da662b
Kiprianas Spiridonovas <34286+kspi@users.noreply.github.com>
2011-11-07 21:23:02 +0200

    Implement rudimentary thread RSS.

--- mail/feeds.py
+++ mail/feeds.py
@@ -2,7 +2,8 @@
 
 from django.contrib.syndication.views import Feed
 from django.utils.translation import ugettext as _
-from models import Thread
+from django.shortcuts import get_object_or_404
+from models import Thread, Message
 from web.utils import summary
 
 
@@ -25,3 +26,32 @@ class ThreadsFeed(Feed):
 
     def item_pubdate(self, item):
         return item.created_at
+
+
+class ThreadFeed(Feed):
+    def get_object(self, request, thread_slug):
+        return get_object_or_404(
+            Thread,
+            is_public=True,
+            slug=thread_slug)
+
+    def title(self, obj):
+        return u'ParašykJiems – {}'.format(obj.subject)
+
+    def description(self, obj):
+        return u''                        # TODO: fill.
+
+    def items(self, obj):
+        return Message.objects.filter(thread=obj).order_by('-date')
+
+    def item_title(self, item):
+        return item.subject
+
+    def item_pubdate(self, item):
+        return item.date
+
+    def item_link(self, item):
+        return item.thread.get_absolute_url()   # TODO: link to specific letter, not thread.
+
+    def item_description(self, item):
+        return u''                        # TODO: fill.
