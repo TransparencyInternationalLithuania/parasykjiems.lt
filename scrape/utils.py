e72bb319538fe97c58c4ed3892c0444ff79fdb9c
Kiprianas Spiridonovas <k.spiridonovas@gmail.com>
2011-11-13 10:37:25 +0200

    Improve/refactor scraping, data update.

--- scrape/utils.py
+++ scrape/utils.py
@@ -1,8 +1,12 @@
+# -*- coding: utf-8 -*-
+
 import time
 import re
-from urllib2 import urlopen
+import random
+import urllib2
 from BeautifulSoup import BeautifulSoup
 import models
+from search.models import Institution, RepresentativeKind
 
 
 _MULTIPLE_SPACES = re.compile(r'\s\s+')
@@ -20,8 +24,13 @@ def email(s):
         return s
 
 
+USER_AGENT = 'ParasykJiems (http://parasykjiems.lt/contact/)'
+
+
 def get_soup(url):
-    response = urlopen(url)
+    headers = {'User-agent': USER_AGENT}
+    request = urllib2.Request(url, headers=headers)
+    response = urllib2.urlopen(request)
     return BeautifulSoup(response, convertEntities=BeautifulSoup.HTML_ENTITIES)
 
 
@@ -30,15 +39,15 @@ def contains_any(s, substrings):
 
 
 def delay():
-    time.sleep(1)
+    time.sleep(min(30, max(1, random.normalvariate(5, 5))))
 
 
 def submit_rep_change(institution, kind,
                       delete=False,
                       name=None, email=None, phone=None, other_info=None):
     rep, created = models.RepresentativeChange.objects.get_or_create(
-        institution=institution,
-        kind_name=kind)
+        institution=Institution.objects.get(name=institution),
+        kind=RepresentativeKind.objects.get(name=kind))
     if delete:
         rep.delete_rep = True
     else:
@@ -55,13 +64,15 @@ def submit_rep_change(institution, kind,
         return None
 
 
-def submit_inst_change(name, email=None, phone=None, other_info=None):
+def submit_inst_change(institution,
+                       email=None, phone=None, other_info=None, address=None):
     inst, created = models.InstitutionChange.objects.get_or_create(
-        name=name)
+        institution=Institution.objects.get(name=institution))
 
     inst.email = email
     inst.phone = phone
     inst.other_info = other_info
+    inst.address = address
 
     if inst.changed():
         inst.save()
--- scrape/views.py
+++ scrape/views.py
@@ -18,8 +18,51 @@ def admin_update(request):
             change.apply_change()
         return redirect(reverse(admin_update))
     else:
-        changes = models.RepresentativeChange.objects.all().order_by(
-            'institution', 'kind_name')
+        changes = []
+
+        for c in models.InstitutionChange.objects.all().order_by(
+            'institution__name'):
+            change = {'id': 'institution_{}'.format(c.id),
+                      'action': 'modify',
+                      'institution': c.institution.name,
+                      'kind': '',
+                      'name_old': '',
+                      'name_changed': False}
+            for field in ['email', 'phone', 'other_info', 'address']:
+                change[field] = getattr(c, field)
+                change[field + '_changed'] = \
+                    getattr(c, field + '_changed')()
+                if change[field + '_changed']:
+                    change[field + '_old'] = \
+                        getattr(c.institution, field)
+
+            changes.append(change)
+
+        for c in models.RepresentativeChange.objects.all().order_by(
+            'institution__name', 'kind__name'):
+            change = {'id': 'representative_{}'.format(c.id),
+                      'institution': c.institution.name,
+                      'kind': c.kind.name,
+                      'address_old': '',
+                      'address_changed': False}
+            if c.delete_rep:
+                change['action'] = 'delete'
+            elif c.rep:
+                change['action'] = 'modify'
+            else:
+                change['action'] = 'add'
+
+            for field in ['name', 'email', 'phone', 'other_info']:
+                change[field] = getattr(c, field)
+                change[field + '_changed'] = \
+                    getattr(c, field + '_changed')()
+                change[field + '_old'] = \
+                    getattr(c.rep, field)
+
+            changes.append(change)
+
+        changes.sort(key=lambda c: [c['institution'], c['kind']])
+
         return render(request, 'views/admin_update.html', {
             'changes': changes,
         })
