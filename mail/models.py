2489836fa3538ad0b508f0df562c564385083fe3
Kiprianas Spiridonovas <34286+kspi@users.noreply.github.com>
2012-02-10 12:32:39 +0200

    Show message id in PDF error message.

--- mail/models.py
+++ mail/models.py
@@ -201,7 +201,8 @@ class Message(models.Model):
         self.body_text = utils.remove_consequentive_empty_lines(
             utils.remove_reply_email(body_text))
         if has_pdf:
-            raise Exception('Message contains PDF, not sending.')
+            raise Exception('Message {id} contains PDF, not sending.'
+                    .format(id=self.id))
 
     @property
     def reply_email(self):
