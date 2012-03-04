cd5917ae284c52d47ddd8869f8289819fb3cdecf
Kiprianas Spiridonovas <34286+kspi@users.noreply.github.com>
2012-03-04 15:05:27 +0200

    Add hint text to filter field.

--- static/js/threads.js
+++ static/js/threads.js
@@ -1,12 +1,25 @@
-$(function(){
-      $('.threads tr a').each(
-          function(i, a){
-              var $a = $(a);
-              $a.parent().parent().click(
-                  function(e){
-                      window.location = $a.attr('href');
-                  });
-          }
-      );
+$(function() {
+  // Make whole table lines clickable.
+  $('.threads tr a').each(function(i, a) {
+    var $a = $(a);
+    $a.parent().parent().click(function(e) {
+      window.location = $a.attr('href');
+    });
+  });
+
+  var $q = $('.filter input[name=q]');
+  function resetq() {
+    if ($q.val() == '') {
+      $q.val(window.EMPTY_FILTER);
+      $q.addClass('empty');
+    }
   }
- );
+  resetq();
+  $q.blur(resetq);
+  $q.focus(function(e) {
+    if ($q.val() == window.EMPTY_FILTER) {
+      $q.val('');
+      $q.removeClass('empty');
+    }
+  });
+});
