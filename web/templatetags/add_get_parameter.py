58a67ec5de69e95ff31804f9e51e2ab824cf5017
Kiprianas Spiridonovas <34286+kspi@users.noreply.github.com>
2012-03-04 18:25:17 +0200

    Fix paging when using typeahead filtering.
    Improve add_get_parameter templatetag.

--- web/templatetags/add_get_parameter.py
+++ web/templatetags/add_get_parameter.py
@@ -1,51 +1,52 @@
-from django import template
-
-from django.template import Library, Node, resolve_variable, TemplateSyntaxError
-from django.template import Context, Template, Node, resolve_variable, TemplateSyntaxError, Variable
-
-register = template.Library()
-
-
 """
-This is custom tag I wrote for myself for solving situations when you have filter form and page
-numbers in the same page. You want to change ?page=.. or add it if it doesn't exist to save
-filter form data while moving through pages.
+This tag adds GET parameters to the current request URL.
 
 Usage: place this code in your application_dir/templatetags/add_get_parameter.py
 In template:
 {% load add_get_parameter %}
-<a href="{% add_get_paramater param1='const_value',param2=variable_in_context %}">
-    Link with modified params
+<a href="{% add_get_paramater param1='val' param2=var %}">
+    Link with modified parameters
 </a>
 
-It's required that you have 'django.core.context_processors.request' in TEMPLATE_CONTEXT_PROCESSORS
-
-URL: http://django.mar.lt/2010/07/add-get-parameter-tag.html
+This template tag requires 'django.core.context_processors.request' in
+TEMPLATE_CONTEXT_PROCESSORS.
 """
 
 
-class AddGetParameter(Node):
+import re
+from django import template
+
+
+register = template.Library()
+
+
+class AddGetParameter(template.Node):
     def __init__(self, values):
         self.values = values
 
     def render(self, context):
-        req = resolve_variable('request',context)
-        params = req.GET.copy()
+        request = template.resolve_variable('request', context)
+        GET = request.GET.copy()
         for key, value in self.values.items():
-            params[key] = Variable(value).resolve(context)
-        return '?%s' %  params.urlencode()
+            if value is None:
+                if key in GET:
+                    del GET[key]
+            else:
+                GET[key] = template.Variable(value).resolve(context)
+
+        return '?' + GET.urlencode()
 
 
 @register.tag
 def add_get_parameter(parser, token):
-    from re import split
-    contents = split(r'\s+', token.contents, 2)[1]
-    pairs = split(r',', contents)
-
+    opts = re.split(r'\s+', token.contents)[1:]
     values = {}
-
-    for pair in pairs:
-        s = split(r'=', pair, 2)
-        values[s[0]] = s[1]
+    for opt in opts:
+        if opt.startswith('-'):
+            k = opt[1:]
+            values[k] = None
+        else:
+            k, v = opt.split('=', 1)
+            values[k] = v
 
     return AddGetParameter(values)
