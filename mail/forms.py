5b2267eb207463ebdb28ce4456bfb54bbec5fd17
Kiprianas Spiridonovas <34286+kspi@users.noreply.github.com>
2012-05-07 10:15:58 +0300

    Make all new threads public.

--- mail/forms.py
+++ mail/forms.py
@@ -4,11 +4,6 @@ from django.utils.translation import ugettext as _
 import settings
 
 
-LETTER_KIND_CHOICES = (('public', _("Open letter, visible to all visitors "
-                                    "of the website.")),
-                       ('private', _("Private letter")))
-
-
 class WriteLetterForm(forms.Form):
     name = forms.CharField(label=_("Your full name"),
                            help_text=_("Don't use abbreviations."),
@@ -28,10 +23,6 @@ class WriteLetterForm(forms.Form):
     subject = forms.CharField(label=_("Subject"),
                               max_length=100,
                               required=True)
-
-    kind = forms.ChoiceField(label=_("Kind of letter"),
-                             widget=widgets.RadioSelect,
-                             choices=LETTER_KIND_CHOICES)
 
     body = forms.CharField(label=_("Message"),
                            widget=forms.widgets.Textarea(),
