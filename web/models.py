37c3bafe7265fb7d8a2b854a6bc768985cd98263
Kiprianas Spiridonovas <k.spiridonovas@gmail.com>
2012-07-01 19:52:06 +0300

    Remove snippet model and data.

--- web/models.py
+++ web/models.py
@@ -33,27 +33,3 @@ class Article(models.Model):
         self.body_rendered = markdown.markdown(
             self.body)
         super(Article, self).save(*args, **kwargs)
-
-
-class Snippet(models.Model):
-    name = models.CharField(max_length=_NAME_LEN,
-                            unique=True,
-                            db_index=True,
-                            verbose_name=_('snippet identifier'))
-
-    body = models.TextField(
-        blank=True,
-        verbose_name=_('content'))
-
-    body_rendered = models.TextField(blank=True)
-
-    def __unicode__(self):
-        return self.name
-
-    class Meta:
-        verbose_name = _('snippet')
-        verbose_name_plural = _('snippets')
-
-    def save(self, *args, **kwargs):
-        self.body_rendered = markdown.markdown(self.body)
-        super(Snippet, self).save(*args, **kwargs)
